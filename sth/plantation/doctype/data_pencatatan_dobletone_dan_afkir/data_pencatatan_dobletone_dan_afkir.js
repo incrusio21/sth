b89e52fe66b1f552ed0e51b19e1df5d58a85a9b0
sth das <abdul.malik.ahmad997@gmail.com>
2025-07-09 07:45:17 +0000

    init: inisialisasi doctype awal

---
+++ sth/plantation/doctype/data_pencatatan_dobletone_dan_afkir/data_pencatatan_dobletone_dan_afkir.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, DAS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Data Pencatatan Dobletone Dan Afkir", {
+// 	refresh(frm) {
+
+// 	},
+// });
