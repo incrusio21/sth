07b79ae848ed4045be149956a04e2a10ebd6bf0f
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-08-05 11:04:38 +0700

    ref(budget perawatan): cukup set ha per tahun krn rotasi dan qty sudah ada pada controller

--- sth/plantation/doctype/budget_perawatan_tahunan/budget_perawatan_tahunan.py
+++ sth/plantation/doctype/budget_perawatan_tahunan/budget_perawatan_tahunan.py
@@ -11,22 +11,12 @@ class BudgetPerawatanTahunan(BudgetController):
 	def validate(self):
 		super().validate()
 		self.set_uom_upah_perawatan()
-		self.set_volume_total()
+		self.set_ha_per_tahun()
 
 	def set_uom_upah_perawatan(self):
 		for d in self.upah_perawatan:
 			d.uom = self.uom
 
-	def set_volume_total(self):
-		table_field = self.get("upah_bibitan" if self.is_bibitan else "upah_perawatan")
-		self.volume_total = self.mean_rotasi = self.ha_per_tahun = 0.0
-		volume_total = mean_rotasi = 0.0
-		
-		for item in table_field:
-			volume_total += item.qty
-			mean_rotasi += item.rotasi
-
-		if table_field:
-			self.volume_total = flt(volume_total)
-			self.mean_rotasi = flt(mean_rotasi/len(table_field))
-			self.ha_per_tahun = flt(self.grand_total / self.volume_total)
+	def set_ha_per_tahun(self):
+		table_field = "upah_bibitan" if self.is_bibitan else "upah_perawatan"
+		self.ha_per_tahun = flt(self.grand_total / self.get(f"{table_field}_qty"))
