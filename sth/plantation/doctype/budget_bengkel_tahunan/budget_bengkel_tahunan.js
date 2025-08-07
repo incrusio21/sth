fb61fe4b76f94790d09f693586a24ec9d4870256
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-08-07 15:40:19 +0700

    feat(budget bengkel): budget bengkel mengikuti budget controller

--- sth/plantation/doctype/budget_bengkel_tahunan/budget_bengkel_tahunan.js
+++ sth/plantation/doctype/budget_bengkel_tahunan/budget_bengkel_tahunan.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Budget Bengkel Tahunan", {
-// 	refresh(frm) {
+sth.plantation.setup_budget_controller()
 
-// 	},
-// });
+frappe.ui.form.on("Budget Bengkel Tahunan", {
+	refresh(frm) {
+
+	},
+    jam_per_tahun(frm){
+        frm.set_value("rp_kmhm", flt(frm.doc.grand_total / frm.doc.jam_per_tahun, precision("rp_kmhm", frm.doc)))
+    }
+});
+
+sth.plantation.BudgetBengkelTahunan = class BudgetBengkelTahunan extends sth.plantation.BudgetController {
+    refresh(doc) {
+        super.refresh(doc)
+    }
+
+    after_calculate_grand_total(){
+        this.frm.doc.rp_kmhm = flt(
+            this.frm.doc.grand_total / this.frm.doc.jam_per_tahun, 
+            precision("rp_kmhm", this.frm.doc)
+        );
+    }
+}
+
+cur_frm.script_manager.make(sth.plantation.BudgetBengkelTahunan);
--- sth/plantation/doctype/budget_bengkel_tahunan/budget_bengkel_tahunan.py
+++ sth/plantation/doctype/budget_bengkel_tahunan/budget_bengkel_tahunan.py
@@ -2,8 +2,13 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.utils import flt
+from sth.controllers.budget_controller import BudgetController
 
+class BudgetBengkelTahunan(BudgetController):
+	def validate(self):
+		super().validate()
+		self.set_rp_per_jam()
 
-class BudgetBengkelTahunan(Document):
-	pass
+	def set_rp_per_jam(self):
+		self.rp_kmhm = flt(self.grand_total / self.jam_per_tahun, self.precision("rp_kmhm"))
