b117cd1e0134b9af386a35676bf20591fc61bcb5
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-08-11 10:25:13 +0700

    feat(budget traksi): tamban kodingan budget controller

--- sth/plantation/doctype/budget_traksi_tahunan/budget_traksi_tahunan.js
+++ sth/plantation/doctype/budget_traksi_tahunan/budget_traksi_tahunan.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Budget Traksi Tahunan", {
-// 	refresh(frm) {
+sth.plantation.setup_budget_controller()
 
-// 	},
-// });
+frappe.ui.form.on("Budget Traksi Tahunan", {
+	refresh(frm) {
+
+	},
+    total_km_hm(frm){
+        frm.set_value("rp_kmhm", flt(frm.doc.grand_total / frm.doc.total_km_hm, precision("rp_kmhm", frm.doc)))
+    }
+});
+
+sth.plantation.BudgetTraksiTahunan = class BudgetTraksiTahunan extends sth.plantation.BudgetController {
+    refresh(doc) {
+        super.refresh(doc)
+    }
+
+    after_calculate_grand_total(){
+        this.frm.doc.rp_kmhm = flt(
+            this.frm.doc.grand_total / this.frm.doc.total_km_hm, 
+            precision("rp_kmhm", this.frm.doc)
+        );
+    }
+}
+
+cur_frm.script_manager.make(sth.plantation.BudgetTraksiTahunan);
--- sth/plantation/doctype/budget_traksi_tahunan/budget_traksi_tahunan.py
+++ sth/plantation/doctype/budget_traksi_tahunan/budget_traksi_tahunan.py
@@ -2,8 +2,15 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.utils import flt
+from sth.controllers.budget_controller import BudgetController
 
 
-class BudgetTraksiTahunan(Document):
-	pass
+class BudgetTraksiTahunan(BudgetController):
+	def validate(self):
+		super().validate()
+		self.set_rp_per_km_hm()
+
+	def set_rp_per_km_hm(self):
+		self.rp_kmhm = flt(self.grand_total / self.total_km_hm, self.precision("rp_kmhm"))
+
