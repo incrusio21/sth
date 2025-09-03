5c3770d809cc5ea05686378ed2e5adb33dafe94a
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-09-03 13:59:10 +0700

    feat(rkb umum): tambahan rkb umum

---
+++ sth/plantation/doctype/rencana_kerja_bulanan_umum/rencana_kerja_bulanan_umum.js
@@ -0,0 +1,54 @@
+// Copyright (c) 2025, DAS and contributors
+// For license information, please see license.txt
+
+sth.plantation.setup_rencana_kerja_controller()
+
+// frappe.ui.form.on("Rencana Kerja Bulanan Umum", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+sth.plantation.RencanaKerjaBulananUmum = class RencanaKerjaBulananUmum extends sth.plantation.RencanaKerjaController {
+    setup(doc) {
+        super.setup(doc)
+
+        let me = this
+        for (const fieldname of ["ump_harian"]) {
+            frappe.ui.form.on(doc.doctype, fieldname, function(doc, cdt, cdn) {
+                me.calculate_total(cdt, cdn, "pegawai")
+            });
+        }
+    }
+
+    set_query_field(){
+        super.set_query_field()
+    
+        this.frm.set_query("kode_kegiatan", function(doc){
+            return{
+                filters: {
+                    company: doc.company,
+                    tipe_kegiatan: "Biaya Umum",
+                    is_group: 0
+                }
+            }
+        })
+
+    }
+
+    premi(_, cdt, cdn){
+        this.calculate_total(cdt, cdn)
+    }
+
+    update_rate_or_qty_value(item){
+        item.rate = this.frm.doc.ump_harian
+    }
+
+    update_value_after_amount(item){
+        super.update_value_after_amount(item)
+
+        item.amount = flt(item.amount + (item.premi || 0), precision("amount", item));
+    }
+}
+
+cur_frm.script_manager.make(sth.plantation.RencanaKerjaBulananUmum);
---
+++ sth/plantation/doctype/rencana_kerja_bulanan_umum/rencana_kerja_bulanan_umum.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2025, DAS and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils import flt
+from sth.controllers.rencana_kerja_controller import RencanaKerjaController
+
+class RencanaKerjaBulananUmum(RencanaKerjaController):
+	def update_rate_or_qty_value(self, item, precision):
+        # set on child class if needed
+		item.rate = self.ump_harian
+
+	def update_value_after_amount(self, item, precision):
+		# set on child class if needed
+		item.amount = flt(item.amount + (item.get("premi") or 0) + (item.get("budget_tambahan") or 0) , precision)
