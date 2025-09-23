c7ca4143b35533c01326a67bb112c47163b82e0a
rickydamarsaputra <retrocode.rc@gmail.com>
2025-09-23 10:01:14 +0700

    feat(pengajuan budget tambahan): update detail table

--- sth/plantation/doctype/pengajuan_budget_tambahan/pengajuan_budget_tambahan.js
+++ sth/plantation/doctype/pengajuan_budget_tambahan/pengajuan_budget_tambahan.js
@@ -7,6 +7,27 @@ frappe.ui.form.on("Pengajuan Budget Tambahan", {
     // frm.set_df_property("kendaraan", "cannot_add_rows", true);
     // frm.set_df_property("angkut", "cannot_add_rows", true);
   },
+  kode_kegiatan(frm) {
+    if (frm.doc.tipe_kegiatan != "Traksi") return
+
+    frappe.db.get_value("Rencana Kerja Bulanan Pengangkutan Panen",
+      {
+        rencana_kerja_bulanan: frm.doc.rencana_kerja_bulanan,
+        kode_kegiatan: frm.doc.kode_kegiatan
+      },
+      ["name", "tonase", "jarak_pks"]
+    ).then(r => {
+      if (r.message) {
+        const { tonase, jarak_pks } = r.message
+        frm.set_value("tonase", tonase);
+        frm.set_value("jarak_pks", jarak_pks);
+
+        frm.refresh_fields()
+      } else {
+        console.log("Data tidak ditemukan");
+      }
+    });
+  },
   target_volume(frm) {
     frm.doc.qty_tenaga_kerja = frm.doc.volume_basis ? flt(frm.doc.target_volume / frm.doc.volume_basis) : 0
     if (frm.doc.target_volume) {
@@ -30,6 +51,55 @@ frappe.ui.form.on("Detail PBT Material", {
   },
 });
 
+frappe.ui.form.on("Detail PBT Kendaraan", {
+  item(frm, cdt, cdn) {
+    let item = locals[cdt][cdn];
+    let km = (frm.doc.tonase / item.kap_kg) * frm.doc.jarak_pks * 2;
+    frappe.model.set_value(cdt, cdn, "qty", km)
+
+    frappe.db.get_value("Alat Berat Dan Kendaraan",
+      { name: item.item },
+      ["name", "tipe_master"]
+    ).then(kendaraanRes => {
+      if (!kendaraanRes.message) return
+      const { tipe_master } = kendaraanRes.message
+
+      if (tipe_master == "Dump Truck") {
+        frappe.db.get_value("Kegiatan",
+          { name: frm.doc.kode_kegiatan },
+          ["rp_basis"]
+        ).then(kegiatanRes => {
+          if (!kegiatanRes.message) return
+          const { rp_basis } = kegiatanRes.message
+
+          frappe.model.set_value(cdt, cdn, "rate", rp_basis)
+        });
+      }
+
+      if (tipe_master == "Alat Berat" || tipe_master == "Kendaraan Umum") {
+        frappe.db.get_value("Company",
+          { name: frm.doc.company },
+          ["name", "ump"]
+        ).then(companyRes => {
+          if (!companyRes.message) return
+          const { ump } = companyRes.message
+
+          frappe.model.set_value(cdt, cdn, "rate", ump)
+        });
+      }
+    });
+  },
+});
+
+frappe.ui.form.on("Detail PBT Pegawai", {
+  qty(frm, cdt, cdn) {
+    let item = locals[cdt][cdn];
+
+    frappe.model.set_value(cdt, cdn, "jumlah_hk", item.qty / frm.doc.volume_basis)
+    frappe.model.set_value(cdt, cdn, "rate", frm.doc.rate_basis)
+  },
+});
+
 sth.plantation.PengajuanBudgetTambahan = class PengajuanBudgetTambahan extends sth.plantation.TransactionController {
   setup(doc) {
     super.setup(doc)
@@ -37,7 +107,7 @@ sth.plantation.PengajuanBudgetTambahan = class PengajuanBudgetTambahan extends s
     let me = this
     for (const fieldname of ["kode_kegiatan", "tipe_kegiatan", "divisi", "blok", "posting_date"]) {
       frappe.ui.form.on(doc.doctype, fieldname, function () {
-        // me.get_rkb_data()
+        me.get_rkb_data()
       });
     }
   }
@@ -69,26 +139,26 @@ sth.plantation.PengajuanBudgetTambahan = class PengajuanBudgetTambahan extends s
   }
 
 
-  // get_rkb_data() {
-  //   let me = this
-  //   let doc = this.frm.doc
-  //   if (!(doc.kode_kegiatan && doc.tipe_kegiatan && doc.divisi && doc.blok && doc.posting_date)) return
+  get_rkb_data() {
+    let me = this
+    let doc = this.frm.doc
+    if (!(doc.kode_kegiatan && doc.tipe_kegiatan && doc.divisi && doc.blok && doc.posting_date)) return
 
-  //   frappe.call({
-  //     method: "sth.plantation.doctype.pengajuan_budget_tambahan.pengajuan_budget_tambahan.get_rencana_kerja_bulanan",
-  //     args: {
-  //       kode_kegiatan: doc.kode_kegiatan,
-  //       tipe_kegiatan: doc.tipe_kegiatan,
-  //       divisi: doc.divisi,
-  //       blok: doc.blok,
-  //       posting_date: doc.posting_date
-  //     },
-  //     freeze: true,
-  //     callback: function (data) {
-  //       me.frm.set_value(data.message)
-  //     }
-  //   })
-  // }
+    frappe.call({
+      method: "sth.plantation.doctype.pengajuan_budget_tambahan.pengajuan_budget_tambahan.get_rencana_kerja_bulanan",
+      args: {
+        kode_kegiatan: doc.kode_kegiatan,
+        tipe_kegiatan: doc.tipe_kegiatan,
+        divisi: doc.divisi,
+        blok: doc.blok,
+        posting_date: doc.posting_date
+      },
+      freeze: true,
+      callback: function (data) {
+        me.frm.set_value(data.message)
+      }
+    })
+  }
 }
 
 cur_frm.script_manager.make(sth.plantation.PengajuanBudgetTambahan);
