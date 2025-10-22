46e80caab9f921da2b4cb13c4aa4c6d7dfc41b6b
sth das <abdul.malik.ahmad997@gmail.com>
2025-10-22 07:03:13 +0000

    ref(kegiatan): hapus field company sebagai autoname

--- sth/plantation/doctype/kegiatan/kegiatan.py
+++ sth/plantation/doctype/kegiatan/kegiatan.py
@@ -2,16 +2,14 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import flt
+from frappe.utils import cstr, flt
 from frappe.utils.nestedset import NestedSet
 
 
 class Kegiatan(NestedSet):
 	
 	def autoname(self):
-		from erpnext.accounts.utils import get_autoname_with_number
-
-		self.name = get_autoname_with_number(self.kd_kgt, self.nm_kgt, self.company)
+		self.name = get_autoname_with_number(self.kd_kgt, self.nm_kgt)
 
 	def validate(self):
 		self.validate_items_company()
@@ -28,8 +26,17 @@ class Kegiatan(NestedSet):
 		if self.is_group:
 			self.material = []
 
+def get_autoname_with_number(number_value, doc_title):
+	"""append title with prefix as number and suffix as company's abbreviation separated by '-'"""
+	parts = [doc_title.strip()]
+
+	if cstr(number_value).strip():
+		parts.insert(0, cstr(number_value).strip())
+
+	return " - ".join(parts)
+
 @frappe.whitelist()
-def get_children(doctype, parent, company, kategori_kegiatan=None, is_root=False):
+def get_children(doctype, parent, kategori_kegiatan=None, is_root=False):
 	from erpnext.accounts.report.financial_statements import sort_accounts
 
 	parent_fieldname = "parent_" + frappe.scrub(doctype)
@@ -39,7 +46,6 @@ def get_children(doctype, parent, company, kategori_kegiatan=None, is_root=False
 	filters.append([f'ifnull(`{parent_fieldname}`,"")', "=", "" if is_root else parent])
 
 	if is_root:
-		filters.append(["company", "=", company])
 		if kategori_kegiatan:
 			filters.append(["kategori_kegiatan", "=", kategori_kegiatan])
 	else:
