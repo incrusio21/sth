4b0c8230a0eab5bf904c8db4472f361c625ae4b6
krisnajufer <krisnajufer07@gmail.com>
2025-11-18 11:01:43 +0700

    UPDATE: fetch perhitungan 70%

--- sth/hr_customize/doctype/perhitungan_kompensasi_phk/perhitungan_kompensasi_phk.js
+++ sth/hr_customize/doctype/perhitungan_kompensasi_phk/perhitungan_kompensasi_phk.js
@@ -1,11 +1,17 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Perhitungan Kompensasi PHK", {
+frappe.ui.form.on("Perhitungan Kompensasi PHK", {
 // 	refresh(frm) {
 
 // 	},
-// });
+    ssa(frm){
+        fetchTableSeym(frm);
+    },
+    dphk(frm){
+        fetchTableSeym(frm);
+    }
+});
 
 sth.plantation.PerhitunganKaryawanPHK = class PerhitunganKaryawanPHK extends sth.plantation.AccountsController {
     refresh() {
@@ -14,3 +20,17 @@ sth.plantation.PerhitunganKaryawanPHK = class PerhitunganKaryawanPHK extends sth
 }
 
 cur_frm.script_manager.make(sth.plantation.PerhitunganKaryawanPHK);
+
+function fetchTableSeym(frm) { 
+    if (!frm.doc.ssa || !frm.doc.dphk) {
+        return
+    }
+    frm.call('fetch_perhitungan', { throw_if_missing: true })
+    .then(r => {
+        if (r.message) {
+            let linked_doc = r.message;
+            // do something with linked_doc
+
+        }
+    })
+}
--- sth/hr_customize/doctype/perhitungan_kompensasi_phk/perhitungan_kompensasi_phk.py
+++ sth/hr_customize/doctype/perhitungan_kompensasi_phk/perhitungan_kompensasi_phk.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from sth.controllers.accounts_controller import AccountsController
+from frappe.utils import month_diff
 
 class PerhitunganKompensasiPHK(AccountsController):
 	def on_submit(self):
@@ -11,3 +13,33 @@ class PerhitunganKompensasiPHK(AccountsController):
 	def on_cancel(self):
 		super().on_cancel()
 		self.make_gl_entry()
+
+	@frappe.whitelist()
+	def fetch_perhitungan(self):
+		date_of_joining = frappe.db.get_value("Employee", self.employee, "date_of_joining")
+		base = frappe.db.get_value("Salary Structure Assignment", self.ssa, "base")
+		setup_dasar_phk_components = frappe.db.get_all("Detail Dasar PHK", {"parent": self.dphk}, "*")
+		list_perhitungan = []
+
+		working_month = month_diff(self.l_date, date_of_joining)
+		for row in setup_dasar_phk_components:
+			perhitungan_component = {
+				"nm": row.nm,
+				"fp": row.fp,
+				"gaji_pokok": base
+			}
+			cond = {
+				"parent": row.nm,
+				"from_month": ['<', working_month],
+				"to_month": ['>', working_month],
+			}
+			setup_komponen_phk = frappe.db.get_value("Setup Komponen PHK", row.nm, "*")
+			pengkali_komponen = frappe.db.get_value("Detail Setup Komponen PHK", cond, "pengkali")
+
+			perhitungan_component.update({"fps": (pengkali_komponen if pengkali_komponen else 0)})
+			result = row.fp * perhitungan_component["fps"] * base
+			# if setup_komponen_phk.is_cuti:
+			perhitungan_component.update({"sbttl": result})
+			self.append("table_seym", perhitungan_component)
+
+		# return list_perhitungan
