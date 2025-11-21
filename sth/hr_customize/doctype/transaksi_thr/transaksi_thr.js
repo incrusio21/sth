731b6079d43bc4922b5bf12fbc3536a5259de039
rickydamarsaputra <retrocode.rc@gmail.com>
2025-11-21 03:51:00 +0000

    feat: update transaksi thr

--- sth/hr_customize/doctype/transaksi_thr/transaksi_thr.js
+++ sth/hr_customize/doctype/transaksi_thr/transaksi_thr.js
@@ -17,6 +17,30 @@ frappe.ui.form.on("Transaksi THR", {
         },
         __("Create"),
       );
+
+      frm.doc.table_employee.forEach(r => {
+        const rule_map = [
+          { key: "UMR", fields: ["umr"] },
+          { key: "GP", fields: ["gaji_pokok"] },
+          { key: "Uang_Daging", fields: ["uang_daging"] },
+          { key: "Natura", fields: ["natura"] },
+          {
+            key: "Jumlah_Bulan_Bekerja",
+            fields: ["jumlah_bulan_bekerja", "masa_kerja"]
+          },
+        ];
+
+        rule_map.forEach(item => {
+          item.fields.forEach(f => {
+            if (r[f] == 0 || r[f] == undefined || r[f] == null) {
+              r[f] = null;
+            }
+            console.log(`${Math.random()} - ${f} - ${r[f]}`)
+          });
+        });
+
+        frm.refresh_field("table_employee");
+      });
     }
 
     frm.set_df_property("table_employee", "cannot_add_rows", true);
@@ -104,18 +128,7 @@ frappe.ui.form.on("Transaksi THR", {
         },
       });
 
-      if (thr_rate.message) {
-        const rule_map = [
-          { key: "UMR", fields: ["umr"] },
-          { key: "GP", fields: ["gaji_pokok"] },
-          { key: "Uang_Daging", fields: ["uang_daging"] },
-          { key: "Natura", fields: ["natura"] },
-          {
-            key: "Jumlah_Bulan_Bekerja",
-            fields: ["jumlah_bulan_bekerja", "masa_kerja"]
-          },
-        ];
-
+      if (thr_rate.message.thr_rule) {
         let row = frm.add_child("table_employee", {
           nik: emp.no_ktp,
           employee: emp.name,
@@ -128,35 +141,133 @@ frappe.ui.form.on("Transaksi THR", {
           bank_name: emp.bank_name,
           designation: emp.designation,
           custom_divisi: emp.custom_divisi,
-          pkp_status: emp.pkp_status,
+          custom_kriteria: emp.custom_kriteria,
           thr_rule: thr_rate.message.thr_rule,
           subtotal: thr_rate.message.subtotal,
         });
 
-        // RESET semua field
-        rule_map.forEach(item => {
-          item.fields.forEach(f => row[f] = null);
-        });
+        const rule_map = [
+          { key: "UMR", fields: ["umr"] },
+          { key: "GP", fields: ["gaji_pokok"] },
+          { key: "Uang_Daging", fields: ["uang_daging"] },
+          { key: "Natura", fields: ["natura"] },
+          {
+            key: "Jumlah_Bulan_Bekerja",
+            fields: ["jumlah_bulan_bekerja", "masa_kerja"]
+          },
+        ];
 
-        // ISI field sesuai rule
         if (row.thr_rule) {
           rule_map.forEach(item => {
             if (row.thr_rule.includes(item.key)) {
               item.fields.forEach(f => {
-                row[f] = thr_rate.message[f];
+                row[f] = thr_rate.message[f] ?? null;
               });
             } else {
               item.fields.forEach(f => {
-                delete row[f];
+                row[f] = null;
               });
             }
           });
         }
-        console.log("Row:", JSON.stringify(row));
+
       }
     }
 
+    // for (const emp of records.message) {
+    //   const thr_rate = await frappe.call({
+    //     method: "get_thr_rate",
+    //     doc: frm.doc,
+    //     args: {
+    //       employee: emp.name,
+    //       pkp_status: emp.pkp_status,
+    //       employee_grade: emp.grade,
+    //       employment_type: emp.employment_type,
+    //       kriteria: emp.custom_kriteria
+    //     },
+    //   });
+
+    //   if (thr_rate.message) {
+    //     let row = frm.add_child("table_employee", {
+    //       nik: emp.no_ktp,
+    //       employee: emp.name,
+    //       employee_name: emp.employee_name,
+    //       date_of_joining: emp.date_of_joining,
+    //       employee_grade: emp.grade,
+    //       employment_type: emp.employment_type,
+    //       custom_kriteria: emp.custom_kriteria,
+    //       bank_ac_no: emp.bank_ac_no,
+    //       bank_name: emp.bank_name,
+    //       designation: emp.designation,
+    //       custom_divisi: emp.custom_divisi,
+    //       custom_kriteria: emp.custom_kriteria,
+    //       thr_rule: thr_rate.message.thr_rule,
+    //       subtotal: thr_rate.message.subtotal,
+    //     });
+
+    //     frm.doc.table_employee.forEach(r => {
+    //       const rule_map = [
+    //         { key: "UMR", fields: ["umr"] },
+    //         { key: "GP", fields: ["gaji_pokok"] },
+    //         { key: "Uang_Daging", fields: ["uang_daging"] },
+    //         { key: "Natura", fields: ["natura"] },
+    //         {
+    //           key: "Jumlah_Bulan_Bekerja",
+    //           fields: ["jumlah_bulan_bekerja", "masa_kerja"]
+    //         },
+    //       ];
+
+    //       if (r.thr_rule) {
+    //         rule_map.forEach(item => {
+    //           if (r.thr_rule.includes(item.key)) {
+    //             item.fields.forEach(f => {
+    //               console.log(emp.employee_name, f, thr_rate.message[f], thr_rate.message);
+    //               r[f] = thr_rate.message[f]
+    //             });
+    //           } else {
+    //             item.fields.forEach(f => {
+    //               r[f] = null
+    //             });
+    //           }
+    //         });
+    //       } else {
+    //         r.gaji_pokok = null;
+    //         r.umr = null;
+    //         r.uang_daging = null;
+    //         r.natura = null;
+    //         r.masa_kerja = null;
+    //         r.jumlah_bulan_bekerja = null;
+    //       }
+    //     });
+    //   }
+
+    //   frm.refresh_field("table_employee");
+    // }
     frm.refresh_field("table_employee");
+
+    frm.doc.table_employee.forEach(r => {
+      const rule_map = [
+        { key: "UMR", fields: ["umr"] },
+        { key: "GP", fields: ["gaji_pokok"] },
+        { key: "Uang_Daging", fields: ["uang_daging"] },
+        { key: "Natura", fields: ["natura"] },
+        {
+          key: "Jumlah_Bulan_Bekerja",
+          fields: ["jumlah_bulan_bekerja", "masa_kerja"]
+        },
+      ];
+
+      rule_map.forEach(item => {
+        item.fields.forEach(f => {
+          if (r[f] == 0 || r[f] == undefined || r[f] == null) {
+            r[f] = null;
+          }
+          console.log(`${Math.random()} - ${f} - ${r[f]}`)
+        });
+      });
+
+      frm.refresh_field("table_employee");
+    });
   },
 });
 
