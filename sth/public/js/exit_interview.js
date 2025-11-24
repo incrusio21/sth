b9845a152599d45e8c79687f6f00e9863a927299
sth das <abdul.malik.ahmad997@gmail.com>
2025-11-24 04:01:20 +0000

    UPDATE: add condition on exit interview

--- sth/public/js/exit_interview.js
+++ sth/public/js/exit_interview.js
@@ -1,11 +1,12 @@
 frappe.ui.form.on("Exit Interview", {
     refresh(frm){
         btnCreatePerhitunganPHK(frm);
+        frm.set_value('ref_doctype', 'Perhitungan Kompensasi PHK');
     }
 })
 
 function btnCreatePerhitunganPHK(frm) {
-    if (frm.doc.reference_document_name && frm.doc.docstatus != 0) {
+    if (frm.doc.reference_document_name && frm.doc.docstatus != 1) {
         return
     }
     frm.add_custom_button('Perhitungan Kompensasi PHK', () => {
