dd5f70be602bf27ae51f4d9820a523006449db6a
Chandra Satria <chandra.invi@gmail.com>
2025-11-27 14:18:04 +0700

    add mutiple kriteria

--- sth/custom/payment_entry.py
+++ sth/custom/payment_entry.py
@@ -2,24 +2,37 @@ import frappe
 
 def cek_kriteria(self,method):
 	if self.references:
-		doctype = self.references[0].get("reference_doctype")
-		
-		if self.dokumen != doctype or not self.dokumen:
-			# kalau tidak sama, kosongin dulu table
-			self.detail_dokumen_finance = []
+		for row in self.references:
+			doctype = row.reference_doctype
+			docname = row.reference_name
 
-		if self.dokumen != doctype:
-			if doctype:
-				fill_kriteria(self, doctype)
+			check = 0
+			for row in self.detail_dokumen_finance:
+				if row.type == doctype and row.name1 == docname:
+					check = 1
 			
-			self.dokumen = doctype
+			if check == 0:
+				fill_kriteria(self, doctype, docname)
 
-def fill_kriteria(self,doctype):
+		# bersih-bersih kalau ada yang tidak di reference
+		list_type = []
+		list_name = []
+
+		for row in self.references:
+			list_type.append(row.reference_doctype)
+			list_name.append(row.reference_name)
+
+		self.detail_dokumen_finance = [baris for baris in self.detail_dokumen_finance if baris.type in list_type and baris.name1 in list_name]
+
+def fill_kriteria(self,doctype, docname):
 	# ambil dulu dari kriteria
 	kriteria = frappe.db.sql(""" SELECT name FROM `tabKriteria Dokumen Finance` WHERE name = "{}" """.format(doctype))
 	if len(kriteria) > 0:
 		kriteria_doc = frappe.get_doc("Kriteria Dokumen Finance",kriteria[0][0])
 		for row in kriteria_doc.kriteria_dokumen_finance:
-			self.append("detail_dokumen_finance",{
-				"rincian_dokumen_finance": row.rincian_dokumen_finance
-			})
+			if row.aktif == 1:
+				self.append("detail_dokumen_finance",{
+					"rincian_dokumen_finance": row.rincian_dokumen_finance,
+					"type": doctype,
+					"name1": docname
+				})
