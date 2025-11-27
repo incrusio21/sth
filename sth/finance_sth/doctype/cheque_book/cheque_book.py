f0e5049bf1a7de4b1f7629862cac6628b6e41370
krisnajufer <krisnajufer07@gmail.com>
2025-11-27 13:52:03 +0700

    UPDATE: cheque book finish every validation and create cheque number

--- sth/finance_sth/doctype/cheque_book/cheque_book.py
+++ sth/finance_sth/doctype/cheque_book/cheque_book.py
@@ -6,21 +6,150 @@ from frappe.model.document import Document
 
 
 class ChequeBook(Document):
-    
-	def on_update(self):
+	def validate(self):
+		self.validate_numbers()
+		self.validate_cheque_number()
+
+	def on_submit(self):
 		self.make_cheque_number()
+	
+	def before_update_after_submit(self):
+		self.validate_cheque_number_updated()
+
+	def on_update_after_submit(self):
+		self.update_cheque_number()
+
+	def before_cancel(self):
+		cheque_numbers = get_cheque_numbers(self.cheque_start_no, self.cheque_end_no)
+		check_usage_cheque_number(self.cheque_code, cheque_numbers.start_no, cheque_numbers.end_no)
+
+	def on_cancel(self):
+		cheque_numbers = get_cheque_numbers(self.cheque_start_no, self.cheque_end_no)
+		delete_cheque_number(self.cheque_code, cheque_numbers.start_no, cheque_numbers.end_no)
+		self.update_total_remaining()
 
 	def make_cheque_number(self):
-		start_no = int(self.cheque_start_no)
-		end_no = int(self.cheque_end_no) + 1
-		cheque_total = end_no - start_no
-		for i in range(start_no, end_no):
-			doc = frappe.get_doc({
-				'doctype': 'Cheque Number',
-				'number': i,
-				'cheque_book': self.name
-			})
-			doc.insert()
+		cheque_numbers = get_cheque_numbers(self.cheque_start_no, self.cheque_end_no)
+		create_cheque_number(self.name, self.cheque_code, cheque_numbers.start_no, cheque_numbers.end_no)
 
-		self.cheques_total = cheque_total
-		self.remaining_cheques = cheque_total
+		self.update_total_remaining()
+
+	def validate_cheque_number(self, is_update=False):
+		cheque_numbers = get_cheque_numbers(self.cheque_start_no, self.cheque_end_no)
+		code = list(set(f"{self.cheque_code}{row}" for row in range(cheque_numbers.start_no, cheque_numbers.end_no)))
+
+		if is_update:
+			old_doc = self.get_doc_before_save()
+			old_end_no = int(old_doc.cheque_end_no)
+			end_no = int(self.cheque_end_no)+1
+			code = list(set(f"{self.cheque_code}{row}" for row in range(old_end_no, end_no)))
+
+		cheque_number = frappe.qb.DocType("Cheque Number")
+		query = (frappe.qb.from_(cheque_number)
+			.select(cheque_number.name)
+			.where(cheque_number.name.isin(code))
+		)
+
+		res = query.run(as_dict=True)
+		if not res:
+			return True
+
+		cheque_exist = list(set(row.name for row in res))
+		str_cheque = ", ".join(cheque_exist)
+		frappe.throw(f"Cheque Number <b>{str_cheque}</b> sudah pernah dibuat di Cheque Book lain")
+
+	def validate_cheque_number_updated(self):
+		old_doc = self.get_doc_before_save()
+		old_end_no = int(old_doc.cheque_end_no) + 1
+		end_no = int(self.cheque_end_no)
+
+		if end_no < old_end_no:
+			check_usage_cheque_number(self.cheque_code, end_no, old_end_no)
+		if end_no > old_end_no:
+			self.validate_cheque_number(is_update=True)
+
+	def update_cheque_number(self):
+		old_doc = self.get_doc_before_save()
+		old_end_no = int(old_doc.cheque_end_no)
+		end_no = int(self.cheque_end_no)
+
+		if end_no < old_end_no:
+			old_end_no += 1
+			delete_cheque_number(self.cheque_code, end_no, old_end_no)
+		if end_no > old_end_no:
+			end_no += 1
+			create_cheque_number(self.name, self.cheque_code, old_end_no, end_no)
+	
+		self.update_total_remaining()
+
+	def update_total_remaining(self):
+		total_cheque_number = frappe.db.count("Cheque Number", {
+											"cheque_book": self.name
+										})
+		used_cheque_number = frappe.db.count("Cheque Number", {
+											"cheque_book": self.name,
+											"reference_doc": ["is", "set"],
+											"reference_name": ["is", "set"]
+										})
+
+		self.cheques_total = total_cheque_number
+		self.remaining_cheques = total_cheque_number - used_cheque_number
+		self.db_update_all()
+
+	def validate_numbers(self):
+		start_no = int(self.cheque_start_no)
+		end_no = int(self.cheque_end_no)
+		warning_no = int(self.cheque_warning_no)
+
+		if start_no > end_no:
+			frappe.throw("<b>Cheque Start Number </b> tidak boleh lebih besar dari <b>Cheque End Number</b>")
+		if warning_no < start_no or warning_no > end_no:
+			frappe.throw("<b>Cheque Waring Number</b> harus diantara <b>Cheque Start Number</b> dan <b>Cheque End Number</b>")
+
+
+def get_cheque_numbers(cheque_start_no, cheque_end_no):
+	start_no = int(cheque_start_no)
+	end_no = int(cheque_end_no) + 1
+
+	return frappe._dict({
+		"start_no": start_no,
+		"end_no": end_no
+	})
+
+def check_usage_cheque_number(cheque_code, start_no, end_no):
+	code = list(set(f"{cheque_code}{row}" for row in range(start_no, end_no)))
+
+	cheque_number = frappe.qb.DocType("Cheque Number")
+	query = (frappe.qb.from_(cheque_number)
+		.select(cheque_number.name)
+		.where((cheque_number.name.isin(code)) & 
+			(cheque_number.reference_doc.notnull()) & 
+			(cheque_number.reference_name.notnull()))
+	)
+	res = query.run(as_dict=True)
+	if not res:
+		return True
+	
+	cheque_is_used = list(set(row.name for row in res))
+	str_cheque = ", ".join(cheque_is_used)
+	frappe.throw(f"Cheque Number <b>{str_cheque}</b> sudah digunakan, mohon periksa kembali")
+
+def create_cheque_number(cheque_book, cheque_code, start_no, end_no):
+	for i in range(start_no, end_no):
+		doc = frappe.get_doc({
+			'doctype': 'Cheque Number',
+			'number': i,
+			'code': cheque_code,
+			'cheque_book': cheque_book
+		})
+		doc.insert()
+
+def delete_cheque_number(cheque_code, start_no, end_no):
+	code = list(set(f"{cheque_code}{row}" for row in range(start_no, end_no)))
+	cheque_number = frappe.qb.DocType("Cheque Number")
+	query = (frappe.qb.from_(cheque_number)
+		.delete()
+		.where(cheque_number.name.isin(code))
+	)
+
+	query.run()
