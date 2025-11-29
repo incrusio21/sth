91d794ef3316c055ca778138ad3cbf1bab9f11ed
krisnajufer <krisnajufer07@gmail.com>
2025-11-29 18:31:13 +0700

    UPDATE: processing data to pdo bahan bakar

--- sth/finance_sth/doctype/pdo_bahan_bakar_vtwo/pdo_bahan_bakar_vtwo.py
+++ sth/finance_sth/doctype/pdo_bahan_bakar_vtwo/pdo_bahan_bakar_vtwo.py
@@ -1,9 +1,65 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PDOBahanBakarVtwo(Document):
 	pass
+
+def process_pdo_bahan_bakar(data, childs):
+	pdo_bahan_bakar = frappe.db.get_value("PDO Bahan Bakar Vtwo", {
+		'reference_doc': data['reference_doc'],
+		'reference_name': data['reference_name']
+	}, "*")
+	
+	if pdo_bahan_bakar:
+		update_pdo_bahan_bakar(pdo_bahan_bakar, data, childs)
+	else:
+		make_pdo_bahan_bakar(data, childs)
+
+def make_pdo_bahan_bakar(data, childs):
+	data.update({
+		"doctype": "PDO Bahan Bakar Vtwo",
+		"naming_series": "BB-"
+	})
+	
+	doc = frappe.get_doc(data)
+	
+	doc = insert_childs(doc, childs)
+	doc.insert(ignore_permissions=True)
+
+	update_transaction_number(doc, data, childs)
+
+def update_pdo_bahan_bakar(pdo_bahan_bakar, data, childs):
+	doc = frappe.get_doc("PDO Bahan Bakar Vtwo", pdo_bahan_bakar.name)
+	if not childs:
+		doc.delete()
+		update_transaction_number(doc, data, childs)
+	else:
+		doc.update(data)
+		doc.set("pdo_bahan_bakar_vtwo", [])
+		doc = insert_childs(doc, childs)
+		doc.save()
+
+def insert_childs(doc, childs):
+	for row in childs:
+		doc.append("pdo_bahan_bakar_vtwo", {
+			"employee": row.employee,
+			"designation": row.designation,
+			"plafon": row.plafon,
+			"unit_price": row.unit_price,
+			"revised_plafon": row.revised_plafon,
+			"revised_unit_price": row.revised_unit_price,
+			"needs": row.needs,
+			"price_total": row.price_total,
+			"revised_price_total": row.revised_price_total,
+		})
+
+	return doc
+
+def update_transaction_number(doc, data, childs):
+	main_pdo = frappe.get_doc(data['reference_doc'], data['reference_name'])
+	main_pdo.bahan_bakar_transaction_number = doc.name if childs else None
+	main_pdo.save()
--- sth/finance_sth/doctype/permintaan_dana_operasional/permintaan_dana_operasional.py
+++ sth/finance_sth/doctype/permintaan_dana_operasional/permintaan_dana_operasional.py
@@ -1,9 +1,38 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+from sth.finance_sth.doctype.pdo_bahan_bakar_vtwo.pdo_bahan_bakar_vtwo import process_pdo_bahan_bakar
+
+PROCESSORS_INSERT = {
+	"Bahan Bakar": process_pdo_bahan_bakar
+}
 
 class PermintaanDanaOperasional(Document):
-	pass
+	def on_update(self):
+		self.process_data_to_insert_vtwo()
+
+	def process_data_to_insert_vtwo(self):
+		pdo_categories = ["Bahan Bakar", "Perjalanan Dinas", "Kas", "Dana Cadangan", "NON PDO"]
+		for pdo in pdo_categories:
+			fieldname = pdo.lower().replace(" ", "_")
+
+			if not self.get(f"pdo_{fieldname}") and not self.get(f"{fieldname}_transaction_number"):
+				continue
+				
+			data = {
+				"grand_total": self.get(f"grand_total_{fieldname}"),
+				"outstanding_amount": self.get(f"outstanding_amount_{fieldname}"),
+				"debit_to": self.get(f"{fieldname}_debit_to"),
+				"credit_to": self.get(f"{fieldname}_credit_to"),
+				"reference_doc": "Permintaan Dana Operasional",
+				"reference_name": self.name,
+			}
+			
+			childs = self.get(f"pdo_{fieldname}")
+			
+			handlers = PROCESSORS_INSERT.get(pdo)
+			if handlers:
+				handlers(data, childs)
