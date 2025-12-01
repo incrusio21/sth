68eb62f18cc706db7325de7863965eac0977e5d8
NurHolisKafi <nurholis195@gmail.com>
2025-12-01 15:42:57 +0700

    fix: add queries controller

--- sth/public/js/sth.bundle.js
+++ sth/public/js/sth.bundle.js
@@ -5,3 +5,4 @@ import "./controllers/budget_controller";
 import "./controllers/rencana_kerja";
 import "./controllers/bkm_controller";
 import "./controllers/komoditi_editor";
+import "./controllers/queries";
