d4a55d6d1634f54cb0d39c8ab1b0e37acd3c2cfa
krisnajufer <krisnajufer07@gmail.com>
2025-12-01 16:28:53 +0700

    UPDATE: revision by gl by child in PDO

--- sth/finance_sth/doctype/pdo_perjalanan_dinas_vtwo/pdo_perjalanan_dinas_vtwo.py
+++ sth/finance_sth/doctype/pdo_perjalanan_dinas_vtwo/pdo_perjalanan_dinas_vtwo.py
@@ -2,7 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
+import erpnext
 
+from frappe import _, scrub
 from sth.controllers.accounts_controller import AccountsController
 
 class PDOPerjalananDinasVtwo(AccountsController):
@@ -16,6 +18,69 @@ class PDOPerjalananDinasVtwo(AccountsController):
 	def on_cancel(self):
 		super().on_cancel()
 		self.make_gl_entry()
+
+	def get_gl_entries(self):
+		gl_entries = []
+
+		self.make_party_gl_entry(gl_entries)
+		self.make_salary_gl_entry(gl_entries)
+
+		return gl_entries
+
+	def make_party_gl_entry(self, gl_entries):
+		against = []
+		datas = self.get('pdo_perjalanan_dinas_vtwo')
+		for row in datas:
+			debit_to = row.get('debit_to')
+			if debit_to in against:
+				continue
+			against.append(debit_to)
+
+		gl_entries.append(
+			self.get_gl_dict(
+				{
+					"account": self.credit_to,
+					"against": ", ".join(against),
+					"credit": self.grand_total,
+					"credit_in_account_currency": self.grand_total,
+					"cost_center": self.cost_center,
+					"party_type": self._party_type,
+					"party": self.get(scrub(self._party_type)),
+					"against_voucher": self.name,
+					"against_voucher_type": self.doctype,	
+				},
+				item=self,
+			)
+		)
+
+	def make_salary_gl_entry(self, gl_entries):
+		cost_center = erpnext.get_default_cost_center(self.company)
+		datas = self.get('pdo_perjalanan_dinas_vtwo')
+		accounts_merge = {}
+
+		for row in datas:
+			debit_to = row.get('debit_to')
+			revised_total = row.get('revised_total')
+
+			if debit_to in accounts_merge:
+				accounts_merge[debit_to] += revised_total
+			else:
+				accounts_merge[debit_to] = revised_total
+
+		for account, total in accounts_merge.items():
+			gl_entries.append(
+				self.get_gl_dict(
+					{
+						"account": account,
+						"against": self.get(scrub(self._party_type)) or self.credit_to,
+						"debit": total,
+						"debit_in_account_currency": total,
+						"cost_center": cost_center,
+					},
+					item=self,
+				)
+			)
+
 
 def process_pdo_perjalanan_dinas(data, childs):
 	pdo_perjalanan_dinas = frappe.db.get_value("PDO Perjalanan Dinas Vtwo", {
@@ -66,6 +131,8 @@ def insert_childs(doc, childs):
 			"needs": row.needs,
 			"total": row.total,
 			"revised_total": row.revised_total,
+			"routine_type": row.routine_type,
+			"debit_to": row.debit_to,
 		})
 
 	return doc
