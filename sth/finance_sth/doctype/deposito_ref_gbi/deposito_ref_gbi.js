011ae456deee8f15b2ade78ff4a3ebb395974e9a
krisnajufer <krisnajufer07@gmail.com>
2025-12-04 22:41:08 +0700

    UPDATE: create doctype deposito ref gbi and modified last deposite reference BRM STH

--- sth/finance_sth/doctype/deposito_ref_gbi/deposito_ref_gbi.js
+++ sth/finance_sth/doctype/deposito_ref_gbi/deposito_ref_gbi.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Deposito", {
+// frappe.ui.form.on("Deposito Ref GBI", {
 // 	refresh(frm) {
 
 // 	},
--- sth/finance_sth/doctype/deposito_ref_gbi/deposito_ref_gbi.py
+++ sth/finance_sth/doctype/deposito_ref_gbi/deposito_ref_gbi.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Deposito(Document):
+class DepositoRefGBI(Document):
 	pass
