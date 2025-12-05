ca77660f363634df3b502f17efba8c2b20b10467
krisnajufer <krisnajufer07@gmail.com>
2025-12-05 14:43:45 +0700

    UPDATE: deposito 70%

--- sth/finance_sth/doctype/deposito/deposito.js
+++ sth/finance_sth/doctype/deposito/deposito.js
@@ -1,8 +1,85 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Deposito", {
-// 	refresh(frm) {
+frappe.ui.form.on("Deposito", {
+	refresh(frm) {
+        filterBankAccount(frm)
+        filterCreditTo(frm)
+	},
+    value_date(frm){
+        calculateTenor(frm) 
+    },
+    maturity_date(frm){
+        calculateTenor(frm) 
+    },
+    interest(frm){
+        calculateDeposito(frm)
+    },
+    tax(frm){
+        calculateDeposito(frm)
+    },
+    deposit_amount(frm){
+        calculateDeposito(frm)
+    }
+});
 
-// 	},
-// });
+function filterBankAccount(frm) {
+    frm.set_query('bank_account', (doc) => {
+        return {
+            filters:{
+                company: ["=", doc.company],
+                bank: ["=", doc.bank]
+            }
+        }
+    })
+}
+
+function calculateTenor(frm) {
+    const startDate = frm.doc.value_date;
+    const endDate = frm.doc.maturity_date;
+    
+    const diffDay = frappe.datetime.get_day_diff(endDate, startDate) + 1
+    const month = Math.floor(diffDay / 30);
+    frm.set_value('tenor', month)
+    frm.refresh_field('tenor')
+
+    calculateDeposito(frm)
+}
+
+function calculateDeposito(frm) {
+    const interest = frm.doc.interest / 100;
+    const tax = frm.doc.tax / 100;
+    const depositAmount = frm.doc.deposit_amount;
+    const startDate = frm.doc.value_date;
+    const endDate = frm.doc.maturity_date;
+    const yearDays = frm.doc.year_days;
+    if (!interest || !tax || !depositAmount || !startDate || !endDate) {
+        return
+    }
+
+    const diffDay = frm.doc.tenor
+    const interestAmount = depositAmount * diffDay * interest/yearDays;
+    const taxAmount = interestAmount * tax;
+    const total = interestAmount - taxAmount;
+
+    frm.set_value("interest_amount", interestAmount)
+    frm.set_value("tax_amount", taxAmount)
+    frm.set_value("total", total)
+    frm.set_value("grand_total", total)
+    frm.set_value("outstanding_amount", total)
+    frm.refresh_field("interest_amount")
+    frm.refresh_field("tax_amount")
+    frm.refresh_field("total")
+    frm.refresh_field("grand_total")
+    frm.refresh_field("outstanding_amount")
+}
+
+function filterCreditTo(frm) {
+    frm.set_query("debit_to", () => {
+        return {
+            filters: {
+                account_type: "Receivable"
+            }
+        }
+    })
+}
--- sth/finance_sth/doctype/deposito/deposito.py
+++ sth/finance_sth/doctype/deposito/deposito.py
@@ -1,9 +1,40 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import date_diff
 
+from sth.controllers.accounts_controller import AccountsController
 
-class Deposito(Document):
-	pass
+class Deposito(AccountsController):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self._expense_account = "expense_account"
+		self._expense_account = "expense_account"
+
+	def validate(self):
+		self.calculate_deposito()
+		self.set_missing_value()
+
+	def on_submit(self):
+		self.make_gl_entry()
+
+	def on_cancel(self):
+		super().on_cancel()
+		self.make_gl_entry()
+
+	def calculate_deposito(self):
+		self.calculate_tenor()
+		interest_amount = self.deposit_amount * self.tenor * (self.interest/100) / self.year_days
+		tax_amount = interest_amount * (self.tax/100)
+		total = interest_amount - tax_amount
+		
+		self.interest_amount = interest_amount
+		self.tax_amount = tax_amount
+		self.total = total
+		self.grand_total = total
+		self.outstanding_amount = total
+
+	def calculate_tenor(self):
+		self.tenor = date_diff(self.maturity_date, self.value_date)+1
