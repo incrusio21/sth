137bfbfc2c15b0a37705f9a7aff3b19c6196188b
NurHolisKafi <nurholis195@gmail.com>
2025-12-09 16:55:21 +0700

    feat: update doctype

--- sth/mill/doctype/timbangan/timbangan.js
+++ sth/mill/doctype/timbangan/timbangan.js
@@ -1,13 +1,27 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Timbangan", {
-// 	refresh(frm) {
+frappe.ui.form.on("Timbangan", {
+    refresh(frm) {
 
-// 	},
-// });
+    },
+
+    set_netto(row) {
+        const bruto = row.bruto || 0
+        const tara = row.tara || 0
+        frappe.model.set_value(row.doctype, row.name, "netto", bruto - tara)
+    }
+});
 
 frappe.ui.form.on("Timbangan Item", {
+    bruto(frm, dt, dn) {
+        var row = locals[dt][dn]
+        frm.events.set_netto(row)
+    },
 
+    tara(frm, dt, dn) {
+        var row = locals[dt][dn]
+        frm.events.set_netto(row)
+    },
 });
 
