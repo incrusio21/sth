538b94417861533dd9351ff09eb5cc61a028126f
krisnajufer <krisnajufer07@gmail.com>
2025-12-09 11:32:31 +0700

    FIX: code calculate interest

--- sth/finance_sth/doctype/deposito/deposito.py
+++ sth/finance_sth/doctype/deposito/deposito.py
@@ -30,10 +30,16 @@ class Deposito(AccountsController):
 		interest_amount = self.deposit_amount * (self.interest/100)  * (self.tenor*self.month_days) / self.year_days
 		tax_amount = interest_amount * (self.tax/100)
 		total = interest_amount - tax_amount
+		interest_amount_monthly = self.deposit_amount * (self.interest/100)  * self.month_days / self.year_days
+		tax_amount_monthly = interest_amount_monthly * (self.tax/100)
+		total_monthly = interest_amount_monthly - tax_amount_monthly
 		
 		self.interest_amount = interest_amount
 		self.tax_amount = tax_amount
 		self.total = total
+		self.interest_amount_monthly = interest_amount_monthly
+		self.tax_amount_monthly = tax_amount_monthly
+		self.total_monthly = total_monthly
 		self.grand_total = total
 		self.outstanding_amount = total
 
