adc14621697266e3123d47800d8c90480ef600e6
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-12-10 15:36:43 +0700

    feat(anggaran dasar): add row saham, pengurus, kriteria berdasarkan field akta d tab masing2

--- sth/legal/doctype/anggaran_dasar/anggaran_dasar.py
+++ sth/legal/doctype/anggaran_dasar/anggaran_dasar.py
@@ -1,14 +1,30 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.utils import flt
 
+from frappe.model.document import Document
 
 class AnggaranDasar(Document):
 	
 	def validate(self):
+		self.validate_mandatory_kriteria()
 		self.clear_akta_list()
+		self.calculate_saham()
+
+	def validate_mandatory_kriteria(self):
+		if not self.kriteria:
+			frappe.throw("Kriteria atleast need One data.")
 
 	def clear_akta_list(self):
-		self.akta = None
+		self.akta_saham = self.akta_pengurus = self.akta_kriteria = None
+
+	def calculate_saham(self):
+		grand_total = 0
+		precision = frappe.get_precision("Detail Form Saham", "amount")
+		for sh in self.saham:
+			sh.amount = flt(sh.rate * sh.qty, precision)
+			grand_total += sh.amount
+
+		self.grand_total = flt(grand_total, self.precision("grand_total"))
