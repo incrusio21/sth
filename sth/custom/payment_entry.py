c3f441993ab0db267db5f0037c8fcc5c5cef8899
krisnajufer <krisnajufer07@gmail.com>
2025-12-14 21:42:34 +0700

    UPDATE: get outstanding add filter unit by Payment Entry

--- sth/custom/payment_entry.py
+++ sth/custom/payment_entry.py
@@ -1,4 +1,14 @@
 import frappe
+import json
+
+from frappe import _, qb
+from frappe.utils import getdate, nowdate
+from erpnext.accounts.doctype.payment_entry.payment_entry import get_orders_to_be_billed, split_invoices_based_on_payment_terms, get_negative_outstanding_invoices
+from erpnext.controllers.accounts_controller import get_supplier_block_status
+from erpnext.accounts.utils import get_account_currency, get_outstanding_invoices
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import get_dimensions
+from erpnext.accounts.party import get_party_account
+from erpnext.setup.utils import get_exchange_rate
 
 from sth.finance_sth.doctype.cheque_number.cheque_number import update_cheque_number_pe
 from sth.finance_sth.doctype.cheque_book.cheque_book import update_cheque_book_pe, delete_cheque_history
@@ -75,3 +85,175 @@ def update_status_deposito(self, method):
 
 def update_status_loan_bank(self, method):
     update_loan_bank_payment_entry(self, method)
+    
+
+@frappe.whitelist()
+def get_outstanding_reference_documents(args, validate=False):
+	if isinstance(args, str):
+		args = json.loads(args)
+
+	if args.get("party_type") == "Member":
+		return
+
+	if not args.get("get_outstanding_invoices") and not args.get("get_orders_to_be_billed"):
+		args["get_outstanding_invoices"] = True
+
+	ple = qb.DocType("Payment Ledger Entry")
+	common_filter = []
+	accounting_dimensions_filter = []
+	posting_and_due_date = []
+
+	# confirm that Supplier is not blocked
+	if args.get("party_type") == "Supplier":
+		supplier_status = get_supplier_block_status(args["party"])
+		if supplier_status["on_hold"]:
+			if supplier_status["hold_type"] == "All":
+				return []
+			elif supplier_status["hold_type"] == "Payments":
+				if (
+					not supplier_status["release_date"]
+					or getdate(nowdate()) <= supplier_status["release_date"]
+				):
+					return []
+
+	party_account_currency = get_account_currency(args.get("party_account"))
+	company_currency = frappe.get_cached_value("Company", args.get("company"), "default_currency")
+
+	# Get positive outstanding sales /purchase invoices
+	condition = ""
+	if args.get("voucher_type") and args.get("voucher_no"):
+		condition = " and voucher_type={} and voucher_no={}".format(
+			frappe.db.escape(args["voucher_type"]), frappe.db.escape(args["voucher_no"])
+		)
+		common_filter.append(ple.voucher_type == args["voucher_type"])
+		common_filter.append(ple.voucher_no == args["voucher_no"])
+
+	# Add cost center condition
+	if args.get("cost_center"):
+		condition += " and cost_center='%s'" % args.get("cost_center")
+		accounting_dimensions_filter.append(ple.cost_center == args.get("cost_center"))
+
+	# dynamic dimension filters
+	active_dimensions = get_dimensions()[0]
+	for dim in active_dimensions:
+		if args.get(dim.fieldname):
+			condition += f" and {dim.fieldname}='{args.get(dim.fieldname)}'"
+			accounting_dimensions_filter.append(ple[dim.fieldname] == args.get(dim.fieldname))
+
+	date_fields_dict = {
+		"posting_date": ["from_posting_date", "to_posting_date"],
+		"due_date": ["from_due_date", "to_due_date"],
+	}
+
+	for fieldname, date_fields in date_fields_dict.items():
+		if args.get(date_fields[0]) and args.get(date_fields[1]):
+			condition += " and {} between '{}' and '{}'".format(
+				fieldname, args.get(date_fields[0]), args.get(date_fields[1])
+			)
+			posting_and_due_date.append(ple[fieldname][args.get(date_fields[0]) : args.get(date_fields[1])])
+		elif args.get(date_fields[0]):
+			# if only from date is supplied
+			condition += f" and {fieldname} >= '{args.get(date_fields[0])}'"
+			posting_and_due_date.append(ple[fieldname].gte(args.get(date_fields[0])))
+		elif args.get(date_fields[1]):
+			# if only to date is supplied
+			condition += f" and {fieldname} <= '{args.get(date_fields[1])}'"
+			posting_and_due_date.append(ple[fieldname].lte(args.get(date_fields[1])))
+
+	if args.get("company"):
+		condition += " and company = {}".format(frappe.db.escape(args.get("company")))
+		common_filter.append(ple.company == args.get("company"))
+
+	outstanding_invoices = []
+	negative_outstanding_invoices = []
+
+	party_account = args.get("party_account")
+
+	# get party account if advance account is set.
+	if args.get("book_advance_payments_in_separate_party_account"):
+		accounts = get_party_account(
+			args.get("party_type"), args.get("party"), args.get("company"), include_advance=True
+		)
+		advance_account = accounts[1] if len(accounts) > 1 else None
+
+		if party_account == advance_account:
+			party_account = accounts[0]
+
+	if args.get("get_outstanding_invoices"):
+		outstanding_invoices = get_outstanding_invoices(
+			args.get("party_type"),
+			args.get("party"),
+			[party_account],
+			common_filter=common_filter,
+			posting_date=posting_and_due_date,
+			min_outstanding=args.get("outstanding_amt_greater_than"),
+			max_outstanding=args.get("outstanding_amt_less_than"),
+			accounting_dimensions=accounting_dimensions_filter,
+			vouchers=args.get("vouchers") or None,
+		)
+
+		outstanding_invoices = split_invoices_based_on_payment_terms(
+			outstanding_invoices, args.get("company")
+		)
+
+		for d in outstanding_invoices:
+			d["exchange_rate"] = 1
+			if party_account_currency != company_currency:
+				if d.voucher_type in frappe.get_hooks("invoice_doctypes"):
+					d["exchange_rate"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "conversion_rate")
+				elif d.voucher_type == "Journal Entry":
+					d["exchange_rate"] = get_exchange_rate(
+						party_account_currency, company_currency, d.posting_date
+					)
+			if d.voucher_type in ("Purchase Invoice"):
+				d["bill_no"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "bill_no")
+
+		# Get negative outstanding sales /purchase invoices
+		if args.get("party_type") != "Employee":
+			negative_outstanding_invoices = get_negative_outstanding_invoices(
+				args.get("party_type"),
+				args.get("party"),
+				args.get("party_account"),
+				party_account_currency,
+				company_currency,
+				condition=condition,
+			)
+
+	# Get all SO / PO which are not fully billed or against which full advance not paid
+	orders_to_be_billed = []
+	if args.get("get_orders_to_be_billed"):
+		orders_to_be_billed = get_orders_to_be_billed(
+			args.get("posting_date"),
+			args.get("party_type"),
+			args.get("party"),
+			args.get("company"),
+			party_account_currency,
+			company_currency,
+			filters=args,
+		)
+
+	data = negative_outstanding_invoices + outstanding_invoices + orders_to_be_billed
+
+	if not data:
+		if args.get("get_outstanding_invoices") and args.get("get_orders_to_be_billed"):
+			ref_document_type = "invoices or orders"
+		elif args.get("get_outstanding_invoices"):
+			ref_document_type = "invoices"
+		elif args.get("get_orders_to_be_billed"):
+			ref_document_type = "orders"
+
+		if not validate:
+			frappe.msgprint(
+				_(
+					"No outstanding {0} found for the {1} {2} which qualify the filters you have specified."
+				).format(
+					_(ref_document_type), _(args.get("party_type")).lower(), frappe.bold(args.get("party"))
+				)
+			)
+
+	for i, row in enumerate(data):
+		unit_trans = frappe.db.get_value(row.voucher_type, row.voucher_no, "unit")
+		if not unit_trans or unit_trans != args.get("unit"):
+			del data[i]
+
+	return data
--- sth/hr_customize/custom/payment_entry.js
+++ sth/hr_customize/custom/payment_entry.js
@@ -2,6 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.off("Payment Entry", "validate_reference_document");
+frappe.ui.form.off("Payment Entry", "get_outstanding_documents");
 frappe.ui.form.on("Payment Entry", {
     refresh(frm) {
         frm.set_query("reference_doctype", "references", function () {
@@ -113,4 +114,115 @@ frappe.ui.form.on("Payment Entry", {
 		}
 	},
 
+	get_outstanding_documents: function (frm, filters, get_outstanding_invoices, get_orders_to_be_billed) {
+		frm.clear_table("references");
+
+		if (!frm.doc.party) {
+			return;
+		}
+
+		frm.events.check_mandatory_to_fetch(frm);
+		var company_currency = frappe.get_doc(":Company", frm.doc.company).default_currency;
+
+		var args = {
+			posting_date: frm.doc.posting_date,
+			company: frm.doc.company,
+			party_type: frm.doc.party_type,
+			payment_type: frm.doc.payment_type,
+			party: frm.doc.party,
+			party_account: frm.doc.payment_type == "Receive" ? frm.doc.paid_from : frm.doc.paid_to,
+			cost_center: frm.doc.cost_center,
+            unit: frm.doc.unit
+		};
+
+		for (let key in filters) {
+			args[key] = filters[key];
+		}
+
+		if (get_outstanding_invoices) {
+			args["get_outstanding_invoices"] = true;
+		} else if (get_orders_to_be_billed) {
+			args["get_orders_to_be_billed"] = true;
+		}
+
+		if (frm.doc.book_advance_payments_in_separate_party_account) {
+			args["book_advance_payments_in_separate_party_account"] = true;
+		}
+
+		frappe.flags.allocate_payment_amount = filters["allocate_payment_amount"];
+
+		return frappe.call({
+			method: "sth.custom.payment_entry.get_outstanding_reference_documents",
+			args: {
+				args: args,
+			},
+			callback: function (r, rt) {
+				if (r.message) {
+					var total_positive_outstanding = 0;
+					var total_negative_outstanding = 0;
+					$.each(r.message, function (i, d) {
+						var c = frm.add_child("references");
+						c.reference_doctype = d.voucher_type;
+						c.reference_name = d.voucher_no;
+						c.due_date = d.due_date;
+						c.total_amount = d.invoice_amount;
+						c.outstanding_amount = d.outstanding_amount;
+						c.bill_no = d.bill_no;
+						c.payment_term = d.payment_term;
+						c.payment_term_outstanding = d.payment_term_outstanding;
+						c.allocated_amount = d.allocated_amount;
+						c.account = d.account;
+
+						if (!in_list(frm.events.get_order_doctypes(frm), d.voucher_type)) {
+							if (flt(d.outstanding_amount) > 0)
+								total_positive_outstanding += flt(d.outstanding_amount);
+							else total_negative_outstanding += Math.abs(flt(d.outstanding_amount));
+						}
+
+						var party_account_currency =
+							frm.doc.payment_type == "Receive"
+								? frm.doc.paid_from_account_currency
+								: frm.doc.paid_to_account_currency;
+
+						if (party_account_currency != company_currency) {
+							c.exchange_rate = d.exchange_rate;
+						} else {
+							c.exchange_rate = 1;
+						}
+						if (in_list(frm.events.get_invoice_doctypes(frm), d.reference_doctype)) {
+							c.due_date = d.due_date;
+						}
+					});
+
+					if (
+						(frm.doc.payment_type == "Receive" && frm.doc.party_type == "Customer") ||
+						(frm.doc.payment_type == "Pay" && frm.doc.party_type == "Supplier") ||
+						(frm.doc.payment_type == "Pay" && frm.doc.party_type == "Employee")
+					) {
+						if (total_positive_outstanding > total_negative_outstanding)
+							if (!frm.doc.paid_amount)
+								frm.set_value(
+									"paid_amount",
+									total_positive_outstanding - total_negative_outstanding
+								);
+					} else if (
+						total_negative_outstanding &&
+						total_positive_outstanding < total_negative_outstanding
+					) {
+						if (!frm.doc.received_amount)
+							frm.set_value(
+								"received_amount",
+								total_negative_outstanding - total_positive_outstanding
+							);
+					}
+				}
+
+				frm.events.allocate_party_amount_against_ref_docs(
+					frm,
+					frm.doc.payment_type == "Receive" ? frm.doc.paid_amount : frm.doc.received_amount,
+					false
+				);
+			},
+		});
+	},
 });
