0b60f7dcc887fc6c52c67261d46bc532c65e8dba
krisnajufer <krisnajufer07@gmail.com>
2025-12-16 15:50:09 +0700

    UPDATE: redeem deposito and pinalti

--- sth/finance_sth/doctype/deposito/deposito.py
+++ sth/finance_sth/doctype/deposito/deposito.py
@@ -2,16 +2,17 @@
 # For license information, please see license.txt
 import math
 import frappe
+import erpnext
 
 from frappe.model.document import Document
-from frappe.utils import add_months, date_diff
+from frappe.utils import add_months, date_diff, nowdate
 from frappe.model.mapper import get_mapped_doc
 from sth.controllers.accounts_controller import AccountsController
+from erpnext.accounts.general_ledger import make_gl_entries
 
 class Deposito(AccountsController):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self._party_type = "Employee"
 		self._expense_account = "non_current_asset"
 
 	def validate(self):
@@ -19,17 +20,20 @@ class Deposito(AccountsController):
 		self.set_missing_value()
 
 	def on_submit(self):
-		self.make_gl_entry()
 		if not self.deposito_interest_table:
 			frappe.throw("Mohon lakukan perhitungan deposito dengan benar sehingga menghasilkan data di tabel")
-		self.make_deposito_interest()
+		self.make_gl_entry()
+		if self.deposito_type in ("Non Roll Over", "Roll Over Pokok"):
+			self.make_deposito_interest()
 
 	def on_cancel(self):
-		super().on_cancel()
-		self.make_gl_entry()
 		if not self.deposito_interest_table:
 			frappe.throw("Mohon lakukan perhitungan deposito dengan benar sehingga menghasilkan data di tabel")
+		super().on_cancel()
+		self.make_gl_entry()
 		self.cancel_deposito_intreset()
+		self.cancel_redeemed_deposito()
+		
 
 	def calculate_deposito_interest_table(self):
 		self.deposito_interest_table = []
@@ -42,6 +46,9 @@ class Deposito(AccountsController):
 			days = date_diff(end_date, start_date)
 			deposit_amount = self.calculate_interest_permonth(start_date, end_date, days, deposit_amount)
 			start_date= end_date
+	
+		self.grand_total = deposit_amount
+		self.outstanding_amount = deposit_amount
 
 	def calculate_interest_permonth(self, value_date, maturity_date, days, deposit_amount):
 		interest = self.interest / 100
@@ -111,10 +118,59 @@ class Deposito(AccountsController):
 
 		self.db_update_all()
 
-	def cancel_deposito_intreset(self):
+	def cancel_deposito_intreset(self, is_redeemed=False):
 		for row in self.deposito_interest_table:
 			doc = frappe.get_doc("Deposito Interest", row.deposito_interest)
+			if is_redeemed and not doc.payment_entry and not row.payment:
+				doc.cancel()
+				row.is_redeemed = "Cancelled"
+				row.db_update_all()
+			elif not is_redeemed:
+				doc.cancel()
+		self.db_update_all()
+
+
+	def cancel_redeemed_deposito(self):
+		if self.redeemed_document:
+			doc = frappe.get_doc("Redeemed Deposito", self.redeemed_document)
 			doc.cancel()
+
+	@frappe.whitelist()
+	def make_redemeed_deposito(self):
+		company = frappe.db.get_value("Company", self.company, "*")
+		grand_total = self.grand_total
+		if self.pinalti > 0:
+			grand_total -= self.pinalti
+
+		values = {
+			"doctype": "Redeemed Deposito",
+			"company": self.company,
+			"currency": self.currency,
+			"posting_date": nowdate(),
+			"unit": self.unit,
+			"customer": frappe.db.get_single_value('Payment Settings', "receivable_customer"),
+			"deposito": self.name,
+			"grand_total": grand_total,
+			"outstanding_amount": grand_total,
+			"cost_center": self.cost_center,
+			"debit_to": company.default_deposito_receivable_account,
+			"non_current_asset": self.non_current_asset,
+		}
+		doc = frappe.get_doc(values)
+		doc.insert(ignore_permissions=True)
+		doc.submit()
+		self.redeemed_document = doc.name
+		self.is_redeemed = "Sudah"
+		self.db_update_all()
+
+	@frappe.whitelist()
+	def make_pinalti_deposito(self, pinalti):
+		self.pinalti = pinalti
+		self.db_update_all()
+		self.cancel_deposito_intreset(is_redeemed=True)
+		make_pinalti_deposito_ledger(self)
+		self.make_redemeed_deposito()
+
 
 @frappe.whitelist()
 def make_payment_entry(source_name, target_doc=None):
@@ -160,31 +216,125 @@ def make_payment_entry(source_name, target_doc=None):
 
 def update_deposito_payment_entry(pe, method):
 	for ref in pe.references:
-		if ref.reference_doctype != "Deposito Interest":
+		if ref.reference_doctype not in ("Deposito Interest", "Deposito", "Redeemed Deposito"):
 			continue
-		doc = frappe.get_doc("Deposito Interest", ref.reference_name)
-		# doc.is_redeemed = "Sudah" if method == "on_submit" else "Belum"
-		doc.payment_entry = pe.name if method == "on_submit" else None
-		doc.total_realization = pe.paid_amount if method == "on_submit" else 0
+		field = {
+			"Deposito Interest": "payment_entry",
+			"Deposito": "payment_pay",
+			"Redeemed Deposito": "payment_entry"
+		}
+		field_pe = field.get(ref.reference_doctype)
+		doc = frappe.get_doc(ref.reference_doctype, ref.reference_name)
+
+		pe_name = pe.name if method == "on_submit" else None
+		doc.db_set(field_pe, pe_name) 
+		if ref.reference_doctype == "Deposito Interest":
+			update_payment_interest(pe, method, doc)
+		if ref.reference_doctype == "Redeemed Deposito":
+			frappe.db.set_value("Deposito", ref.reference_name, "payment_received", pe_name)
 		doc.db_update_all()
 
-		values = {
+def update_is_redeemed(parenttype, parent, childtype):
+	child_count = frappe.db.count(childtype, {"parenttype": parenttype, "parent": parent})
+	child_pay_count = frappe.db.count(childtype, {"parenttype": parenttype, "parent": parent, "payment": ["!=", None]})
+	
+	doc = frappe.get_doc(parenttype, parent)
+	doc.is_redeemed = "Sudah" if child_count == child_pay_count else "Belum"
+	doc.db_update_all()
+	
+
+def update_payment_interest(pe, method, doc):
+	doc.total_realization = pe.paid_amount if method == "on_submit" else 0
+	values = {
 			"total_realization" : doc.total_realization,
 			"payment" : doc.payment_entry,
 			"is_redeemed": "Sudah" if method == "on_submit" else "Belum"
 		}
-		frappe.db.set_value(doc.reference_detail_doc, doc.reference_detail_name, values)
+	frappe.db.set_value(doc.reference_detail_doc, doc.reference_detail_name, values)
 
-		total_realization = pe.paid_amount if method == "on_submit" else pe.paid_amount * -1
-		deposito = frappe.get_doc("Deposito", doc.reference_name)
-		deposito.total_realization = deposito.total_realization + total_realization
-		deposito.db_update_all()
-		update_is_redeemed(doc.reference_doc, doc.reference_name, doc.reference_detail_doc)
+	total_realization = pe.paid_amount if method == "on_submit" else pe.paid_amount * -1
+	deposito = frappe.get_doc("Deposito", doc.reference_name)
+	deposito.total_realization = deposito.total_realization + total_realization
+	deposito.db_update_all()
+	update_is_redeemed(doc.reference_doc, doc.reference_name, doc.reference_detail_doc)
 
-def update_is_redeemed(parenttype, parent, childtype):
-    child_count = frappe.db.count(childtype, {"parenttype": parenttype, "parent": parent})
-    child_pay_count = frappe.db.count(childtype, {"parenttype": parenttype, "parent": parent, "payment": ["!=", None]})
-    
-    doc = frappe.get_doc(parenttype, parent)
-    doc.is_redeemed = "Sudah" if child_count == child_pay_count else "Belum"
-    doc.db_update_all()
+@frappe.whitelist()
+def make_principal_payment(source_name, target_doc=None, type=None):
+	doctype = "Redeemed Deposito" if type == "Receive" else "Deposito"
+	account_type = "debit_to" if type == "Receive" else "credit_to"
+	paid_type = "paid_from" if type == "Receive" else "paid_to"
+	def post_process(source, target):
+		party_type = "Customer" if type == "Receive" else "Employee"
+		payment_type = "Receive" if type == "Receive" else "Pay"
+		field_default_party_type = "receivable_customer" if party_type == "Customer" else "internal_employee"
+		default_party = frappe.db.get_single_value("Payment Settings", field_default_party_type)
+		field_party_name = "customer_name" if party_type == "Customer" else "employee_name"
+
+		party_name = frappe.db.get_value(party_type, default_party, field_party_name)
+		company = frappe.db.get_value("Company", source.company, "*")
+		account_currency = frappe.db.get_value("Account", source.debit_to, "account_currency")
+
+		target.payment_type = payment_type
+		target.party_type = party_type
+		target.party = default_party
+		target.party_name = party_name
+		target.paid_amount = source.outstanding_amount
+		target.bank_account = source.bank_account
+		target.paid_from_account_currency = account_currency
+		target.paid_to_account_currency = company.default_currency
+		target.internal_employee = 1 if party_type == "Employee" else 0
+		target.append("references", {
+			"reference_doctype": doctype,
+			"reference_name": source.name,
+			"total_amount": source.grand_total,
+			"outstanding_amount": source.outstanding_amount,
+			"allocated_amount": source.outstanding_amount
+		})
+
+	doclist = get_mapped_doc(
+		doctype,
+		source_name,
+		{
+			doctype: {
+				"doctype": "Payment Entry",
+				"field_map": {
+					account_type: paid_type,
+				}
+			}
+		},
+  		target_doc,
+		post_process,
+	)
+ 
+	return doclist
+
+def make_pinalti_deposito_ledger(parent):
+	company = frappe.db.get_value("Company", parent.company, "*")
+	gl_entries = [
+		frappe._dict({
+			"posting_date": nowdate(),
+			"account": company.default_deposito_expense_account,
+			"debit": parent.pinalti,
+			"credit": 0,
+			"company": parent.company,
+			"voucher_type": "Deposito",
+			"voucher_no": parent.name,
+			"cost_center": erpnext.get_default_cost_center(parent.company) if not parent.cost_center else parent.cost_center
+		}),
+		frappe._dict({
+			"posting_date": nowdate(),
+			"account": company.default_deposito_nca_account,
+			"debit": 0,
+			"credit": parent.pinalti,
+			"company": parent.company,
+			"voucher_type": "Deposito",
+			"voucher_no": parent.name,
+			"cost_center": erpnext.get_default_cost_center(parent.company) if not parent.cost_center else parent.cost_center
+		})
+	]
+
+	make_gl_entries(
+		gl_entries,
+		cancel=False,
+		adv_adj=False
+	)
