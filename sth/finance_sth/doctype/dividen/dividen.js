e68cada77c7e6245906dfc3cf38849e989bd4028
krisnajufer <krisnajufer07@gmail.com>
2025-12-17 16:45:19 +0700

    PEND: dividen

--- sth/finance_sth/doctype/dividen/dividen.js
+++ sth/finance_sth/doctype/dividen/dividen.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Dividen", {
-// 	refresh(frm) {
+frappe.ui.form.on("Dividen", {
+	refresh(frm) {
+        setFilterOne(frm)
+	},
+    company(frm){
+        setFilterOne(frm)
+    },
+    unit_one(frm){
+        setFilterOne(frm)
+    }
+});
 
-// 	},
-// });
+function setFilterOne(frm) {
+    frm.set_query("unit_one", (doc)=>{
+        return {
+            filters:{
+                company: doc.company_one
+            }
+        }
+    })
+    frm.set_query("bank_account_one", (doc)=>{
+        return {
+            filters:{
+                company: doc.company_one,
+                unit: doc.unit_one,
+                bank: doc.bank_one
+            }
+        }
+    })
+}
