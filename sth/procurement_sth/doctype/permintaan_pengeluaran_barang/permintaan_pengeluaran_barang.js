930fd05da4aecec4786520ffef9f991b6c374cad
NurHolisKafi <nurholis195@gmail.com>
2025-12-19 15:10:59 +0700

    fix: add filters and fetch

--- sth/procurement_sth/doctype/permintaan_pengeluaran_barang/permintaan_pengeluaran_barang.js
+++ sth/procurement_sth/doctype/permintaan_pengeluaran_barang/permintaan_pengeluaran_barang.js
@@ -1,8 +1,42 @@
 // Copyright (c) 2025, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Permintaan Pengeluaran Barang", {
-// 	refresh(frm) {
+frappe.ui.form.on("Permintaan Pengeluaran Barang", {
+    setup(frm) {
+        frm.set_query("gudang", function (doc) {
+            return {
+                filters: {
+                    is_group: 0,
+                    company: doc.pt_pemilik_barang,
+                }
+            }
+        })
 
-// 	},
-// });
+        frm.set_query("sub_unit", function (doc) {
+            return {
+                filters: {
+                    company: doc.pt_pemilik_barang,
+                }
+            }
+        })
+    },
+
+    refresh(frm) {
+
+    },
+
+    sub_unit(frm) {
+        if (!frm.doc.sub_unit) {
+            return
+        }
+        frappe.xcall("frappe.client.get_value", {
+            doctype: "Blok",
+            fieldname: ["name"],
+            filters: {
+                unit: frm.doc.sub_unit
+            }
+        }).then((res) => {
+            frm.set_value("blok", res.name)
+        })
+    }
+});
