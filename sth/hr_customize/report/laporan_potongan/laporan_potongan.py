8dd2ededb13608376e9cc7190c185b34947a79b5
rickydamarsaputra <retrocode.rc@gmail.com>
2025-12-22 03:35:30 +0000

    feat: report laporan potongan employee

---
+++ sth/hr_customize/report/laporan_potongan/laporan_potongan.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2025, DAS and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+def execute(filters=None):
+	conditions = get_condition(filters)
+	columns = get_columns(filters)
+	data = []
+
+	query_l_potongan = frappe.db.sql("""
+		SELECT
+		epd.employee as `id`,
+		epd.employee_name as nama,
+		epd.rate as jumlah_potongan
+		FROM `tabEmployee Potongan` as ep
+		JOIN `tabEmployee Potongan Details` as epd ON epd.parent = ep.name
+		JOIN `tabEmployee` as e ON e.name = epd.employee
+		WHERE ep.name IS NOT NULL {};
+  """.format(conditions), filters, as_dict=True)
+
+	for potongan in query_l_potongan:
+		data.append(potongan)
+
+	return columns, data
+
+def get_condition(filters):
+	conditions = ""
+
+	if filters.get("no_transaksi"):
+		conditions += " AND ep.name = %(no_transaksi)s"
+
+	if filters.get("pt"):
+		conditions += " AND ep.company = %(pt)s"
+
+	if filters.get("unit"):
+		conditions += " AND e.unit = %(unit)s"
+
+	if filters.get("golongan"):
+		conditions += " AND e.grade = %(golongan)s"
+
+	if filters.get("potongan"):
+		conditions += " AND ep.jenis_potongan = %(potongan)s"
+
+	return conditions
+
+def get_columns(filters):
+	columns = [
+		{
+			"label": _("ID"),
+			"fieldtype": "Data",
+			"fieldname": "id",
+		},
+		{
+			"label": _("Nama"),
+			"fieldtype": "Data",
+			"fieldname": "nama",
+		},
+		{
+			"label": _("Jumlah Potongan"),
+			"fieldtype": "Currency",
+			"fieldname": "jumlah_potongan",
+		},
+	]
+
+	return columns
