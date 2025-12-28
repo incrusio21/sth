63ad0375679ad3ac77c5ba917a4a0bc2991816ae
sth das <abdul.malik.ahmad997@gmail.com>
2025-12-28 17:29:09 +0000

    update setup bpjs

--- sth/hr_customize/doctype/daftar_bpjs/daftar_bpjs.js
+++ sth/hr_customize/doctype/daftar_bpjs/daftar_bpjs.js
@@ -1,8 +1,40 @@
-// Copyright (c) 2025, DAS and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Daftar BPJS", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Daftar BPJS', {
+    refresh: function(frm) {
+    	console.log(frm.doc.daftar_bpjs_employee)
+        if (frm.doc.docstatus == 0 && frm.doc.daftar_bpjs_employee.length == 0)  {
+        	console.log("TES")
+            frm.add_custom_button(__('Get Employee'), function() {
+                frm.disable_save();
+                frappe.show_alert({
+                    message: __('Loading employees, please wait...'),
+                    indicator: 'blue'
+                }, 5);
+                
+                frappe.call({
+                    doc: frm.doc,
+                    method: 'get_employee',
+                    freeze: true,
+                    freeze_message: __('Fetching employee data...'),
+                    callback: function(r) {
+                        frm.enable_save();
+                        frm.refresh_fields();
+                        
+                        if (r.message) {
+                            frappe.show_alert({
+                                message: __('Successfully loaded {0} employee(s)', [r.message]),
+                                indicator: 'green'
+                            }, 5);
+                        }
+                    },
+                    error: function() {
+                        frm.enable_save();
+                        frappe.show_alert({
+                            message: __('Error loading employees'),
+                            indicator: 'red'
+                        }, 5);
+                    }
+                });
+            });
+        }
+    }
+});
