4eb7e1415a743f0e2c9b2282fe4b2c99ad23d959
sth das <abdul.malik.ahmad997@gmail.com>
2026-01-01 10:08:31 +0000

    js untuk supplier

--- sth/public/js/supplier.js
+++ sth/public/js/supplier.js
@@ -1,18 +1,34 @@
 frappe.ui.form.on('Supplier', {
-    onload: function(frm) {
-        if (frm.is_new() && !frm.doc.kode_supplier) {
-            generate_kode_supplier(frm);
-        }
-    },
+	onload: function(frm) {
+		if (frm.is_new() && !frm.doc.kode_supplier) {
+			generate_kode_supplier(frm);
+		}
+	},
+	refresh: function(frm) {
+		if (frm.is_new()) {
+			frm.set_value('aktif', 1); // Set checkbox checked by default
+			frm.set_value('default', 'Ya');
+			frm.set_value('status_bank', 'Aktif');
+		}
+	},
+	aktif: function(frm) {
+		if (frm.doc.aktif) {
+			frm.set_value('default', 'Ya');
+			frm.set_value('status_bank', 'Aktif');
+		} else {
+			frm.set_value('default', 'Tidak');
+			frm.set_value('status_bank', 'Tidak Aktif');
+		}
+	}
 });
 
 function generate_kode_supplier(frm) {
-    frappe.call({
-        method: 'sth.overrides.supplier.get_next_supplier',
-        callback: function(r) {
-            if (r.message) {
-                frm.set_value('kode_supplier', r.message);
-            }
-        }
-    });
+	frappe.call({
+		method: 'sth.overrides.supplier.get_next_supplier',
+		callback: function(r) {
+			if (r.message) {
+				frm.set_value('kode_supplier', r.message);
+			}
+		}
+	});
 }
