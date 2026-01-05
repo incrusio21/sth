1d3f267e15a4a1e04aee307b5f92e624e38ea943
Chandra Satria <chandra.invi@gmail.com>
2026-01-05 17:09:17 +0700

    update sl untuk absent

--- sth/overrides/salary_slip.py
+++ sth/overrides/salary_slip.py
@@ -125,7 +125,7 @@ class SalarySlip(SalarySlip):
 				self.holiday_days += 1
 				hari_leave += 1
 		
-		self.payment_days = self.total_working_days - self.holiday_days + hari_leave
+		self.payment_days = self.total_working_days - self.holiday_days + hari_leave - self.absent_days
 
 
 	def _get_not_out_attendance_days(self) -> float:
