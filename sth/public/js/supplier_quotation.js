02746f3c22707c821028bf15af54d493b66534d5
sth das <abdul.malik.ahmad997@gmail.com>
2026-01-13 02:32:38 +0000

    fix: change layout and add custom trigger field

--- sth/public/js/supplier_quotation.js
+++ sth/public/js/supplier_quotation.js
@@ -1,16 +1,21 @@
 frappe.ui.form.on("Supplier Quotation", {
-    onload(frm) {
-        let original_refresh = frm.cscript.refresh;
+    setup(frm) {
+        sth.form.override_class_function(frm.cscript, "calculate_totals", () => {
+            frm.trigger("set_value_dpp_and_taxes")
+        })
 
-        frm.cscript.refresh = function () {
-            original_refresh.apply(this, arguments);
-            frm.remove_custom_button("Material Request", "Get Items From")
-            frm.remove_custom_button("Request for Quotation", "Get Items From")
-            if (frm.doc.docstatus === 0) {
-                btn_get_material_request(frm)
-                btn_get_rfq(frm)
-            }
-        };
+        sth.form.override_class_function(frm.cscript, "refresh", () => {
+            frm.trigger("create_custom_buttons")
+        })
+
+    },
+    onload(frm) {
+
+        frm._default_coa = {}
+
+        frappe.xcall("sth.custom.purchase_invoice.get_default_coa", { type: "ppn", company: frm.doc.company }).then((res) => {
+            frm._default_coa.ppn = res
+        })
     },
     refresh(frm) {
         frm.page.btn_secondary.hide()
@@ -26,6 +31,27 @@ frappe.ui.form.on("Supplier Quotation", {
 
         } else {
             frm.remove_custom_button(__("Re open"))
+        }
+    },
+
+    set_value_dpp_and_taxes(frm) {
+        frm.doc.dpp = frm.doc.net_total
+        frm.doc.pph = frm.doc.taxes_and_charges_deducted
+        for (const row of frm.doc.taxes) {
+            if (row.account_head == frm._default_coa.ppn) {
+                frm.doc.ppn = row.tax_amount
+            }
+        }
+        frm.doc.biaya_lainnya = frm.doc.taxes_and_charges_added - frm.doc.ppn
+        frm.refresh_fields()
+    },
+
+    create_custom_buttons(frm) {
+        frm.remove_custom_button("Material Request", "Get Items From")
+        frm.remove_custom_button("Request for Quotation", "Get Items From")
+        if (frm.doc.docstatus === 0) {
+            btn_get_material_request(frm)
+            btn_get_rfq(frm)
         }
     }
 })
@@ -58,7 +84,7 @@ function btn_get_material_request(frm) {
             setTimeout(() => {
                 // console.log(d.dialog);
                 d.dialog.set_value("allow_child_item_selection", 1)
-            }, 300);
+            }, 1000);
 
         },
         __("Get Items From")
@@ -96,7 +122,7 @@ function btn_get_rfq(frm) {
             setTimeout(() => {
                 // console.log(d.dialog);
                 d.dialog.set_value("allow_child_item_selection", 1)
-            }, 300);
+            }, 1000);
         },
         __("Get Items From")
     );
