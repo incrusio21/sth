a5bd740b514c639b85fcb9bc89ab433499cc58fe
NurHolisKafi <nurholis195@gmail.com>
2026-01-13 13:32:42 +0700

    feat: enhance purchase order and supplier quotation functionality with new fields and calculations

--- sth/buying_sth/custom/purchase_order.js
+++ sth/buying_sth/custom/purchase_order.js
@@ -130,6 +130,9 @@ erpnext.utils.update_progress_received = function (opts) {
 frappe.ui.form.on("Purchase Order", {
 	setup(frm) {
 		sth.form.setup_fieldname_select(frm, "items")
+		sth.form.override_class_function(frm.cscript, "calculate_totals", () => {
+			frm.trigger("set_value_dpp_and_taxes")
+		})
 	},
 	refresh(frm) {
 		frm.set_query("purchase_type", () => {
@@ -168,5 +171,17 @@ frappe.ui.form.on("Purchase Order", {
 				});
 			}
 		}
-	}
+	},
+
+	set_value_dpp_and_taxes(frm) {
+		frm.doc.dpp = frm.doc.net_total
+		frm.doc.pph = frm.doc.taxes_and_charges_deducted
+		for (const row of frm.doc.taxes) {
+			if (row.account_head == frm._default_coa.ppn) {
+				frm.doc.ppn = row.tax_amount
+			}
+		}
+		frm.doc.biaya_lainnya = frm.doc.taxes_and_charges_added - frm.doc.ppn
+		frm.refresh_fields()
+	},
 });
