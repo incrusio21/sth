25448b5706c49c5ee39cd9d599edfe6274c126a5
sth das <abdul.malik.ahmad997@gmail.com>
2026-01-23 02:54:07 +0000

    feat: add function create pinv and add necessary field

--- sth/sales_sth/doctype/pengakuan_pembelian_tbs/pengakuan_pembelian_tbs.js
+++ sth/sales_sth/doctype/pengakuan_pembelian_tbs/pengakuan_pembelian_tbs.js
@@ -1,39 +1,97 @@
 frappe.ui.form.on('Pengakuan Pembelian TBS', {
-    setup(frm) {
-        frm.set_query("unit", (doc) => {
-            return {
-                filters: {
-                    company: doc.company
-                }
-            }
-        })
-    },
-    get_data: function (frm) {
-        if (!frm.doc.nama_supplier) {
-            frappe.msgprint(__('Please select a supplier first'));
-            return;
-        }
+	setup(frm) {
+		frm.set_query("unit", (doc) => {
+			return {
+				filters: {
+					company: doc.company
+				}
+			}
+		})
+	},
+	get_data: function (frm) {
+		if (!frm.doc.nama_supplier) {
+			frappe.msgprint(__('Please select a supplier first'));
+			return;
+		}
 
-        frm.call("get_timbangan")
-            .then((res) => {
-                frappe.model.sync(res);
-                frm.refresh();
-            })
-    },
+		frm.call("get_timbangan")
+			.then((res) => {
+				frappe.model.sync(res);
+				frm.refresh();
+			})
+	},
 
-    jarak(frm) {
-        const jarak = frm.doc.jarak
-        if (jarak) {
-            const method = frappe.model.get_server_module_name(frm.doctype) + "." + "get_rate"
-            frappe
-                .xcall(method, { jarak })
-                .then((res) => {
-                    frm.set_value("harga", res + frm.doc.subsidi_angkut)
-                })
-        }
-    },
+	jarak(frm) {
+		const jarak = frm.doc.jarak
+		if (jarak) {
+			const method = frappe.model.get_server_module_name(frm.doctype) + "." + "get_rate"
+			frappe
+				.xcall(method, { jarak })
+				.then((res) => {
+					frm.set_value("harga", res + frm.doc.subsidi_angkut)
+				})
+		}
+	},
 
-    subsidi_angkut(frm) {
-        frm.set_value("harga", frm.doc.harga + frm.doc.subsidi_angkut)
-    }
+	subsidi_angkut(frm) {
+		frm.set_value("harga", frm.doc.harga + frm.doc.subsidi_angkut)
+	},
+
+	jarak_ring: function(frm) {
+		set_jarak_price_list(frm);
+	},
+	
+	unit: function(frm) {
+		set_jarak_price_list(frm);
+	}
 });
+
+function set_jarak_price_list(frm) {
+	if (frm.doc.unit && frm.doc.jarak_ring) {
+		// Format: unit - jarak_ring
+		let price_list_name = `${frm.doc.unit} - ${frm.doc.jarak_ring}`;
+		
+		// Set jarak field with the price list name
+		frm.set_value('jarak', price_list_name);
+		
+		// Check if price list exists
+		frappe.call({
+			method: 'frappe.client.get_value',
+			args: {
+				doctype: 'Price List',
+				filters: { name: price_list_name },
+				fieldname: 'name'
+			},
+			callback: function(r) {
+				if (!r.message) {
+					// Price list doesn't exist, create it
+					create_price_list(frm, price_list_name);
+				}
+			}
+		});
+	}
+}
+
+function create_price_list(frm, price_list_name) {
+	frappe.call({
+		method: 'frappe.client.insert',
+		args: {
+			doc: {
+				doctype: 'Price List',
+				price_list_name: price_list_name,
+				enabled: 1,
+				buying: 1,
+				selling: 0,
+				currency: frappe.defaults.get_default('currency') || 'IDR'
+			}
+		},
+		callback: function(r) {
+			if (r.message) {
+				// frappe.show_alert({
+				// 	message: __('Price List "{0}" created successfully', [price_list_name]),
+				// 	indicator: 'green'
+				// }, 5);
+			}
+		}
+	});
+}
--- sth/sales_sth/doctype/pengakuan_pembelian_tbs/pengakuan_pembelian_tbs.py
+++ sth/sales_sth/doctype/pengakuan_pembelian_tbs/pengakuan_pembelian_tbs.py
@@ -3,6 +3,7 @@ import frappe
 from frappe.utils import today,flt
 from frappe.model.document import Document
 from frappe import _
+from frappe.model.mapper import get_mapped_doc
 
 class PengakuanPembelianTBS(Document):
 	@frappe.whitelist()
@@ -23,3 +24,50 @@ class PengakuanPembelianTBS(Document):
 @frappe.whitelist()
 def get_rate(jarak):
 	return frappe.get_value("Item Price",{"item_code":"TBS","price_list": jarak},["price_list_rate"])
+
+@frappe.whitelist()
+def make_purchase_invoice(source_name, target_doc=None):
+    
+    def set_missing_values(source, target):
+        target.run_method("set_missing_values")
+        target.run_method("calculate_taxes_and_totals")
+    
+    def update_item(source, target, source_parent):
+        target.qty = source.qty
+        target.rate = source.rate
+        
+    doclist = get_mapped_doc(
+        "Pengakuan Pembelian TBS",
+        source_name,
+        {
+            "Pengakuan Pembelian TBS": {
+                "doctype": "Purchase Invoice",
+                "field_map": {
+                    "nama_supplier": "supplier",
+                    "unit": "unit",
+                },
+                "validation": {
+                    "docstatus": ["=", 1]
+                }
+            },
+            "Pengakuan Pembelian TBS Item": {  # Child table name
+                "doctype": "Purchase Invoice Item",
+                "field_map": {
+                    "item_code": "item_code",
+                    "item_name": "item_name",
+                    "qty": "qty",
+                    "uom": "uom",
+                    "rate": "rate",
+                    "amount": "amount",
+                    # Add other item field mappings
+                    # "warehouse": "warehouse",
+                    # "expense_account": "expense_account",
+                },
+                "postprocess": update_item
+            }
+        },
+        target_doc,
+        set_missing_values
+    )
+    
+    return doclist
