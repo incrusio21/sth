7ceb84a24ecd342d50ecbcfaaea6f9537679e0b6
sth das <abdul.malik.ahmad997@gmail.com>
2026-01-28 06:11:52 +0000

    feat: update field and method tax rate

--- sth/accounting_sth/doctype/tax_rate/tax_rate.js
+++ sth/accounting_sth/doctype/tax_rate/tax_rate.js
@@ -1,8 +1,35 @@
 // Copyright (c) 2026, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tax Rate", {
-// 	refresh(frm) {
+frappe.ui.form.on("Tax Rate", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+	onload: function (frm) {
+		frm.set_query("account", "tax_rate_account", function (doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			return {
+				"filters": {
+					"company": row.company,
+					"account_type": "Tax"
+				},
+			};
+		});
+	},
+});
+
+
+frappe.ui.form.on("Tax Rate Account", {
+	company: function(frm){
+		frm.set_query("account", "tax_rate_account", function (doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			return {
+				"filters": {
+					"company": row.company,
+					"account_type": "Tax"
+				},
+			};
+		});
+	}
+});
+
