d594d15524aa7ff08db1de28246ca34b0a31cd29
sth das <abdul.malik.ahmad997@gmail.com>
2026-01-30 03:22:07 +0000

    feat: add client script for PE

--- sth/hr_customize/custom/payment_entry.js
+++ sth/hr_customize/custom/payment_entry.js
@@ -3,79 +3,147 @@
 
 frappe.ui.form.off("Payment Entry", "validate_reference_document");
 frappe.ui.form.off("Payment Entry", "get_outstanding_documents");
+frappe.ui.form.off("Payment Entry", "party");
+
 frappe.ui.form.on("Payment Entry", {
-    refresh(frm) {
-        frm.set_query("reference_doctype", "references", function () {
-            return {
-                query: "sth.hr_customize.custom.payment_entry.get_payment_reference",
-                filters: { 
-                    party_type: frm.doc.party_type 
-                },
-            };
-        });
-        
-        frm.set_query('custom_cheque_number', () => {
-            return {
-                filters: {
-                    reference_doc: ["=", null], 
-                    reference_name: ["=", null], 
-                    bank_account: ["=", frm.doc.bank_account] 
-                }
-            }
-        })
-        
-        frm.set_query('unit', (doc) => {
-            return {
-                filters: {
-                    company: ["=", doc.company],
-                }
-            }
-        })
+	party: function (frm) {
+		if (frm.doc.contact_email || frm.doc.contact_person) {
+			frm.set_value("contact_email", "");
+			frm.set_value("contact_person", "");
+		}
+		if (frm.doc.payment_type && frm.doc.party_type && frm.doc.party && frm.doc.company) {
+			if (!frm.doc.posting_date) {
+				frappe.msgprint(__("Please select Posting Date before selecting Party"));
+				frm.set_value("party", "");
+				return;
+			}
+			frm.set_party_account_based_on_party = true;
 
-        frm.set_query('reference_name', "references", (doc) => {
-            return {
-                filters: {
-                    docstatus: 1,
-                    company: ["=", doc.company],
-                    unit: doc.unit   
-                }
-            }
-        })
+			let company_currency = frappe.get_doc(":Company", frm.doc.company).default_currency;
 
-        frm.set_query('bank_account', (doc) => {
-            return {
-                filters: {
-                    unit: ["=", doc.unit],
-                    company: ["=", doc.company],
-                }
-            }
-        })
-        frm.ignore_doctypes_on_cancel_all = ["Deposito Interest", "Deposito", "Disbursement Loan", "Installment Loan", "Payment Voucher Kas"];
-    },
-	payment_type(frm){
-        frm.set_value("bank_account", "")
-    },
-    party_type(frm){
-        frm.set_value("internal_employee", 0)
-    },
-    unit(frm){
-        if(!frm.doc.unit) return
-        
-        // frm.clear_table("references")
-        // frm.refresh_field("references")
-    },
-    internal_employee(frm){
-        if(!frm.doc.internal_employee) return
+			return frappe.call({
+				method: "erpnext.accounts.doctype.payment_entry.payment_entry.get_party_details",
+				args: {
+					company: frm.doc.company,
+					party_type: frm.doc.party_type,
+					party: frm.doc.party,
+					date: frm.doc.posting_date,
+					cost_center: frm.doc.cost_center,
+				},
+				callback: function (r, rt) {
+					if (r.message) {
+						frappe.run_serially([
+							() => {
+								if (frm.doc.payment_type == "Receive") {
+									frm.set_value("paid_from", r.message.party_account);
+									frm.set_value(
+										"paid_from_account_currency",
+										r.message.party_account_currency
+									);
+									frm.set_value("paid_from_account_balance", r.message.account_balance);
+								} else if (frm.doc.payment_type == "Pay") {
+									frm.set_value("paid_to", r.message.party_account);
+									frm.set_value(
+										"paid_to_account_currency",
+										r.message.party_account_currency
+									);
+									frm.set_value("paid_to_account_balance", r.message.account_balance);
+								}
+							},
+							() => frm.set_value("party_balance", r.message.party_balance),
+							() => frm.set_value("party_name", r.message.party_name),
 
-        frappe.call({
-            method: "sth.hr_customize.custom.payment_entry.get_internal_employee",
-            callback(data){
-                frm.set_value("party", data.message)
-            }
-        })
-    },
+							() => frm.events.hide_unhide_fields(frm),
+							() => frm.events.set_dynamic_labels(frm),
+							() => {
+								frm.set_party_account_based_on_party = false;
+								if (r.message.party_bank_account) {
+									frm.set_value("party_bank_account", r.message.party_bank_account);
+								}
+								if (r.message.bank_account) {
+									frm.set_value("bank_account", r.message.bank_account);
+								}
+							},
+							() =>
+								frm.events.set_current_exchange_rate(
+									frm,
+									"source_exchange_rate",
+									frm.doc.paid_from_account_currency,
+									company_currency
+								),
+							() =>
+								frm.events.set_current_exchange_rate(
+									frm,
+									"target_exchange_rate",
+									frm.doc.paid_to_account_currency,
+									company_currency
+								),
+						]);
+					}
+				},
+			});
+		}
+	},
+	refresh(frm) {
+		frm.set_query("reference_doctype", "references", function () {
+			return {
+				query: "sth.hr_customize.custom.payment_entry.get_payment_reference",
+				filters: {
+					party_type: frm.doc.party_type
+				},
+			};
+		});
 
-    validate_reference_document: function (frm, row) {
+		frm.set_query('custom_cheque_number', () => {
+			return {
+				filters: {
+					reference_doc: ["=", null],
+					reference_name: ["=", null],
+					bank_account: ["=", frm.doc.bank_account]
+				}
+			}
+		})
+
+		frm.set_query('unit', (doc) => {
+			return {
+				filters: {
+					company: ["=", doc.company],
+				}
+			}
+		})
+
+		frm.set_query('bank_account', (doc) => {
+			return {
+				filters: {
+					unit: ["=", doc.unit],
+					company: ["=", doc.company],
+				}
+			}
+		})
+		frm.ignore_doctypes_on_cancel_all = ["Deposito Interest", "Deposito", "Disbursement Loan", "Installment Loan", "Payment Voucher Kas"];
+	},
+	payment_type(frm) {
+		frm.set_value("bank_account", "")
+	},
+	party_type(frm) {
+		frm.set_value("internal_employee", 0)
+	},
+	unit(frm) {
+		if (!frm.doc.unit) return
+
+	},
+	internal_employee(frm) {
+		if (!frm.doc.internal_employee) return
+
+		frappe.call({
+			method: "sth.hr_customize.custom.payment_entry.get_internal_employee",
+			callback(data) {
+				frm.set_value("party", data.message)
+			}
+		})
+	},
+
+	validate_reference_document: function (frm, row) {
 		var _validate = function (i, row) {
 			if (!row.reference_doctype) {
 				return;
@@ -135,7 +203,7 @@ frappe.ui.form.on("Payment Entry", {
 			party: frm.doc.party,
 			party_account: frm.doc.payment_type == "Receive" ? frm.doc.paid_from : frm.doc.paid_to,
 			cost_center: frm.doc.cost_center,
-            unit: frm.doc.unit
+			unit: frm.doc.unit
 		};
 
 		for (let key in filters) {
@@ -229,3 +297,71 @@ frappe.ui.form.on("Payment Entry", {
 		});
 	},
 });
+
+frappe.ui.form.on("Payment Entry References", {
+	refresh(frm,cdt,cdn){
+		frm.set_query('reference_name', "references", function(doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			
+			if (row.reference_doctype) {
+				let filters = {
+					docstatus: 1,
+					company: doc.company
+				};
+				
+				if (row.reference_doctype === "Purchase Invoice" || row.reference_doctype === "Purchase Order") {
+					if (doc.party_type === "Supplier" && doc.party) {
+						filters.supplier = doc.party;
+					}
+				}
+				else if (row.reference_doctype === "Sales Invoice" || row.reference_doctype === "Sales Order") {
+					if (doc.party_type === "Customer" && doc.party) {
+						filters.customer = doc.party;
+					}
+				}
+				else if (row.reference_doctype === "Journal Entry") {
+					if (doc.party_type && doc.party) {
+						filters.pay_to_recd_from = doc.party;
+					}
+				}
+				
+				return {
+					filters: filters
+				};
+			}
+		});
+	},
+	reference_doctype(frm,cdt,cdn){
+		frm.set_query('reference_name', "references", function(doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			
+			if (row.reference_doctype) {
+				let filters = {
+					docstatus: 1,
+					company: doc.company
+				};
+				
+				if (row.reference_doctype === "Purchase Invoice" || row.reference_doctype === "Purchase Order") {
+					if (doc.party_type === "Supplier" && doc.party) {
+						filters.supplier = doc.party;
+					}
+				}
+				else if (row.reference_doctype === "Sales Invoice" || row.reference_doctype === "Sales Order") {
+					if (doc.party_type === "Customer" && doc.party) {
+						filters.customer = doc.party;
+					}
+				}
+				else if (row.reference_doctype === "Journal Entry") {
+					if (doc.party_type && doc.party) {
+						filters.pay_to_recd_from = doc.party;
+					}
+				}
+				
+				return {
+					filters: filters
+				};
+			}
+		});
+	}
+	
+})
