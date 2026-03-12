5920f27950f6896bedf718531e6602b609f00e63
sth das <abdul.malik.ahmad997@gmail.com>
2026-03-12 04:28:00 +0000

    debug_company

--- sth/mill/doctype/pemeriksaan_temperatur_tangki__tangki/pemeriksaan_temperatur_tangki__tangki.js
+++ sth/mill/doctype/pemeriksaan_temperatur_tangki__tangki/pemeriksaan_temperatur_tangki__tangki.js
@@ -7,56 +7,55 @@
 // 	},
 // });
 
+
+
 frappe.ui.form.on("Pemeriksaan Temperatur Tangki- Tangki", {
 
     temperatur_cot: function(frm) {
         check_temp(frm, "temperatur_cot");
     },
+
     temperatur_pot: function(frm) {
         check_temp(frm, "temperatur_pot");
     },
+
     temperatur_cst: function(frm) {
         check_temp(frm, "temperatur_cst");
     },
+
     temperatur_stt: function(frm) {
         check_temp(frm, "temperatur_stt");
     },
+
     temperatur_st: function(frm) {
         check_temp(frm, "temperatur_st");
     }
 
 });
 
+
 function check_temp(frm, fieldname) {
 
-    let value = parseFloat(frm.doc[fieldname]);
-    if (!value && value !== 0) return;
+    let value = frm.doc[fieldname];
+
+    if (!value) return;
 
     frappe.db.get_single_value("Mill Settings", fieldname).then((standard) => {
 
-        let std = parseFloat(standard);
-        if (isNaN(std)) return;
+        if (!standard) return;
 
-        let selisih = value - std;
+        let selisih = value - standard;
+
         let info_field = fieldname + "_info";
 
         if (selisih > 0) {
-            frm.set_value(
-                info_field,
-                `<span style="color:red;font-weight:bold">${Math.abs(selisih)}° di atas standar</span>`
-            );
-        }
+            frm.set_value(info_field, selisih + "° Diatas Standard");
+        } 
         else if (selisih < 0) {
-            frm.set_value(
-                info_field,
-                `<span style="color:blue;font-weight:bold">${Math.abs(selisih)}° di bawah standar</span>`
-            );
-        }
+            frm.set_value(info_field, Math.abs(selisih) + "° Dibawah Standard");
+        } 
         else {
-            frm.set_value(
-                info_field,
-                `<span style="color:green;font-weight:bold">Sesuai standar</span>`
-            );
+            frm.set_value(info_field, "");
         }
 
     });
