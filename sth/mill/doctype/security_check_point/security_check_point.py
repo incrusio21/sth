bc2ea1483bde9ca5b3bcc99d897b49df834db461
sth das <abdul.malik.ahmad997@gmail.com>
2026-03-16 06:20:41 +0000

    feat: update doctype module mill

--- sth/mill/doctype/security_check_point/security_check_point.py
+++ sth/mill/doctype/security_check_point/security_check_point.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
-from doctest import debug
 
 import frappe
 from frappe.model.document import Document
