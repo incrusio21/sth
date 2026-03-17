c5f56be313319e3072c128bf9adef406511837c8
NurHolisKafi <nurholis195@gmail.com>
2026-03-17 14:30:26 +0700

    feat: update get data

--- sth/mill/doctype/data_tbs/data_tbs.py
+++ sth/mill/doctype/data_tbs/data_tbs.py
@@ -20,30 +20,32 @@ class DataTBS(Document):
 				(tbs_olah + tbs_mentah + jumlah_restan_tbs_masak + jumlah_loading_ramp) as grand_total_lori
 			from `tabMonitoring TBS Olah` mto
 			where mto.docstatus = 1 and tgl = %s
-		""",(today()),as_dict=True)
+		""",(self.tanggal_produksi),as_dict=True)
 		data_lori = data_lori[0] if data_lori else {}
 		self.update(data_lori)
 
-		self.jumlah_tbs_restan = get_total_restan()
-		self.jumlah_tbs_diterima = get_total_tbs()
+		self.jumlah_tbs_restan = get_total_restan(self.tanggal_produksi)
+		self.jumlah_tbs_diterima = get_total_tbs(self.tanggal_produksi)
 		self.grand_total_tbs = self.jumlah_tbs_restan + self.jumlah_tbs_diterima
 
 		self.berat_rata_rata_tbs = self.grand_total_tbs / self.grand_total_lori if self.grand_total_lori else 0
 		self.tbs_olah = self.berat_rata_rata_tbs * self.jumlah_lori_olah
 		self.tbs_restan = self.berat_rata_rata_tbs * (self.jumlah_lori_mentah + self.jumlah_lori_masak)
 		self.tbs_loading_ramp = self.berat_rata_rata_tbs * self.lori_estimasi_loading_ramp
+		self.total_jam_olah = frappe.db.get_value("CBC Monitoring",{"docstatus": 1, "posting_date": self.tanggal_produksi},"total_hour_meter")
+		self.kapasitas_pabrik = self.tbs_olah / self.total_jam_olah / 1000
 
-def get_total_tbs():
+def get_total_tbs(tanggal):
 	query = frappe.db.sql("""
 		select sum(balance_qty) as qty from `tabTBS Ledger Entry` tle
 		where tle.item_code = 'TBS' and tle.is_cancelled = 0 and tle.type LIKE 'TBS%%' and tle.posting_date = %s
 		group by tle.item_code
-	""",(today()),as_dict=True)
+	""",(tanggal),as_dict=True)
 
 	return query[0].qty if query else 0
 
-def get_total_restan():
-	tanggal = add_days(today(),-1)
+def get_total_restan(tanggal):
+	tanggal = add_days(tanggal,-1)
 
 	query = frappe.db.sql("""
 		select (tbs_restan + tbs_loading_ramp) as qty from `tabData TBS` dtb
