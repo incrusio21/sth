5c5f660f5191ef71fc92411323424c3af8c71829
sth das <abdul.malik.ahmad997@gmail.com>
2026-04-23 07:01:29 +0000

    feat: add scan qr  station

--- sth/mill/doctype/pemeriksaan_ampere_motor/pemeriksaan_ampere_motor.js
+++ sth/mill/doctype/pemeriksaan_ampere_motor/pemeriksaan_ampere_motor.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2026, DAS and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Pemeriksaan Ampere Motor", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Pemeriksaan Ampere Motor", {
+    onload(frm) {
+        if (frm.is_new()) {
+            new frappe.ui.Scanner({
+                dialog: true,
+                multiple: false,
+                on_scan(data) {
+                    if (data && data.result && data.result.text) {
+                        frm.set_value("stasiun", data.result.text);
+                        frm.set_value("tanggal_scan", frappe.datetime.get_today())
+                        frm.set_value("jam_scan", moment().format('HH:mm:ss'))
+                        frm.set_value("user_scan", frappe.session.user)
+                    }
+                },
+            })
+        }
+    },
+});
--- sth/mill/doctype/pemeriksaan_mesin_turbin/pemeriksaan_mesin_turbin.js
+++ sth/mill/doctype/pemeriksaan_mesin_turbin/pemeriksaan_mesin_turbin.js
@@ -2,17 +2,33 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Pemeriksaan Mesin Turbin", {
+	onload(frm) {
+		if (frm.is_new()) {
+			new frappe.ui.Scanner({
+				dialog: true,
+				multiple: false,
+				on_scan(data) {
+					if (data && data.result && data.result.text) {
+						frm.set_value("stasiun", data.result.text);
+						frm.set_value("tanggal_scan", frappe.datetime.get_today())
+						frm.set_value("jam_scan", moment().format('HH:mm:ss'))
+						frm.set_value("user_scan", frappe.session.user)
+					}
+				},
+			})
+		}
+	},
 	refresh(frm) {
 
 	},
-	hm_awal_turbin(frm){
+	hm_awal_turbin(frm) {
 		hitung_hm(frm)
 	},
-	hm_akhir_turbin(frm){
+	hm_akhir_turbin(frm) {
 		hitung_hm(frm)
 	},
 });
 
-function hitung_hm(frm){
+function hitung_hm(frm) {
 	frm.set_value("total_hm_turbin", frm.doc.hm_akhir_turbin - frm.doc.hm_awal_turbin)
 }
